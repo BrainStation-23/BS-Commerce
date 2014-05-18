e174a50d3e06c8780fb304770c0929ab64e6b6e0
Drew Fyock <fyockm@gmail.com>
2014-05-17 20:03:45 -0400

    #442 dynamic dropdown for submenus

--- packages/articles/app.js
+++ packages/articles/app.js
@@ -18,14 +18,17 @@ Articles.register(function(app, auth, database) {
 
     //We are adding a link to the main menu for all authenticated users
     Articles.menus.add({
-        'roles': ['authenticated'],
         'title': 'Articles',
-        'link': 'all articles'
-    });
-    Articles.menus.add({
         'roles': ['authenticated'],
-        'title': 'Create New Article',
-        'link': 'create article'
+        'dropdown': [{
+            'title': 'Articles list',
+            'link': 'all articles'
+        }, {
+            'divider': true
+        }, {
+            'title': 'Create New Article',
+            'link': 'create article'
+        }]
     });
 
     /*
