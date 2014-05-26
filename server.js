14803f062f2bdca4a44e94483882613bea8095cf
Yonatan Ellman <yonatan@nodeside.com>
2014-05-26 16:11:21 +0300

    removing bootstrap file

--- server.js
+++ server.js
@@ -18,12 +18,17 @@ var mean = require('meanio');
 var options = {};
 
 mean.app(options, function(app, config) {
-	
+
 	console.log('Mean app started on port ' + config.port + ' (' + process.env.NODE_ENV + ')');
 
 	// Initializing logger
 	logger.init(app, passport, mongoose);
-
+	
+	// Register auth dependency
+	mean.register('auth', function() {
+		return require('./server/routes/middlewares/authorization');
+	});
+	
 	// Expose app
 	exports = module.exports = app;
 });
