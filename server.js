cc014810d37dc6734d45b924ba8364ccf92b68c8
Drew Fyock <fyockm@gmail.com>
2014-06-10 00:04:42 -0400

    remove mean-logger

--- server.js
+++ server.js
@@ -3,33 +3,22 @@
 /**
  * Module dependencies.
  */
-var mongoose = require('mongoose'),
-	passport = require('passport'),
-	logger = require('mean-logger');
+var mean = require('meanio');
 
 /**
  * Main application entry file.
  **/
-
-// Initializing system variables
-
-var mean = require('meanio');
-
 var options = {};
-
 
 mean.app(options, function(app, config) {
 
-	console.log('Mean app started on port ' + config.port + ' (' + process.env.NODE_ENV + ')');
+    console.log('Mean app started on port ' + config.port + ' (' + process.env.NODE_ENV + ')');
 
-	// Initializing logger
-	logger.init(app, passport, mongoose);
-	
-	// Register auth dependency
-	mean.register('auth', function() {
-		return require('./server/routes/middlewares/authorization');
-	});
-	
-	// Expose app
-	exports = module.exports = app;
+    // Register auth dependency
+    mean.register('auth', function() {
+        return require('./server/routes/middlewares/authorization');
+    });
+
+    // Expose app
+    exports = module.exports = app;
 });
