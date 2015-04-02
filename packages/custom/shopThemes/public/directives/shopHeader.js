ad1c0d1ec3bd221bbd84500ae22953ab363f680d
Zahid <Zahid@BrainStation-23.com>
2015-04-03 04:16:29 +0600

    Removed default MEAN.IO header and implemented login actions in e-shopper header

---
+++ packages/custom/shopThemes/public/directives/shopHeader.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('mean.shopThemes').directive('shopHeader',['$rootScope', 'Global', function($rootScope, Global){
+  return {
+    templateUrl: '/shopThemes/views/header.html',
+    link: function(scope, element, attrs){
+      scope.global = Global;
+
+      $rootScope.$on('loggedin', function() {
+        scope.global = {
+          authenticated: !! $rootScope.user,
+          user: $rootScope.user
+        };
+      });
+    }
+  };
+}]);
