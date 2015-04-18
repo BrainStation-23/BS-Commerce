fa303114f3e3beac54da37640ac57983021d2a07
saif <saifbuet@gmail.com>
2015-04-18 06:24:58 +0600

    # shop slider arrow position fixed, but transition still not working, # hack used from this link http://stackoverflow.com/questions/20137900/controller-carousel-required-by-directive-ngtransclude-cant-be-found

--- packages/custom/shopWeb/public/shopWeb.js
+++ packages/custom/shopWeb/public/shopWeb.js
@@ -4,3 +4,12 @@ angular.module('mean.shopWeb', ['mean.system'])
     .config(['$viewPathProvider', function($viewPathProvider) {
         $viewPathProvider.override('system/views/index.html', 'shopWeb/views/index.html');
     }]);
+
+
+angular.module('ui.bootstrap.carousel', ['ui.bootstrap.transition'])
+    .controller('CarouselController', ['$scope', '$timeout', '$transition', '$q', function ($scope, $timeout, $transition, $q) {
+    }]).directive('carousel', [function() {
+        return {
+
+        }
+    }]);
