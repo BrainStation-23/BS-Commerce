ddc1b5746cae52ff7af7787772a25f3a54d7cd30
saif <saifbuet@gmail.com>
2015-04-21 03:58:47 +0600

    Category Tree nodes to Category edit state reference successfully working.

--- packages/custom/shopAdmin/public/assets/js/directives/shopAdminCategoryTree.js
+++ packages/custom/shopAdmin/public/assets/js/directives/shopAdminCategoryTree.js
@@ -1,21 +1,24 @@
 'use strict';
 
-angular.module('mean.shopAdmin').directive('shopAdminCategoryTree', ['Global', '$http',
-    function(Global, $http) {
-        return{
+
+angular.module('mean.shopAdmin').directive('shopAdminCategoryTree', ['Global', '$http', '$state',
+    function (Global, $http, $state) {
+        return {
             replace: true,
             template: '<div id="category_jstree_div"></div>',
-            link: function(scope, element, attrs){
+            link: function (scope, element, attrs) {
+
                 var catTree = [];
+
                 $http.get('/api/categories').
-                    success(function(data, status, headers, config) {
-                        for(var i in data){
+                    success(function (data, status, headers, config) {
+                        for (var i in data) {
                             var item = {};
                             item.id = data[i]._id;
                             item.parent = '#';
                             item.text = data[i].name;
                             catTree.push(item);
-                            for(var j in data[i].subCategories){
+                            for (var j in data[i].subCategories) {
                                 var subItem = {};
                                 subItem.id = data[i].subCategories[j]._id;
                                 subItem.parent = data[i]._id;
@@ -23,11 +26,23 @@ angular.module('mean.shopAdmin').directive('shopAdminCategoryTree', ['Global', '
                                 catTree.push(subItem);
                             }
                         }
-                        element.jstree({ 'core' : {
-                            'data' : catTree
-                        } });
+
+                        element.jstree({
+                            'core': {
+                                'data': catTree,
+                                'dblclick_toggle': false
+                            }
+                        })
+                            .
+                            on('activate_node.jstree', function (e, data) {
+                                console.log(angular.toJson($state.get()));
+                                $state.go('Category.Edit', {catId: data.node.id});
+                                //window.location.href = '/admin#!/Category/Edit/'+data.node.id;
+                            });
+
+
                     }).
-                    error(function(data, status, headers, config) {
+                    error(function (data, status, headers, config) {
                         // called asynchronously if an error occurs
                         // or server returns response with an error status.
                     });
@@ -36,3 +51,5 @@ angular.module('mean.shopAdmin').directive('shopAdminCategoryTree', ['Global', '
         };
     }
 ]);
+
+
--- packages/custom/shopAdmin/public/assets/js/shopAdmin.js
+++ packages/custom/shopAdmin/public/assets/js/shopAdmin.js
@@ -20,34 +20,6 @@ angular.module('mean.shopAdmin').config(['$viewPathProvider', '$meanStateProvide
                 templateUrl: 'shopAdmin/views/anonymous.html',
                 controller: 'registrationController'
             });
-            /*.state('Category', {
-                abstract: true,
-                url: '/Category',
-                template: '<ui-view/>'
-            })
-            .state('Category.List', {
-                url: '/List',
-                templateUrl: 'shopAdmin/views/category/list.html',
-                controller: 'categoryListController'
-            })
-            .state('Category.Tree', {
-                url: '/Tree',
-                templateUrl: 'shopAdmin/views/category/tree.html',
-                controller: 'categoryTreeController'
-            })
-            .state('Category.Edit', {
-                url: '/Edit/:catId',
-                params: {
-                    catId: null
-                },
-                templateUrl: 'shopAdmin/views/category/edit.html',
-                controller: 'categoryEditController'
-            })
-            .state('test', {
-                url: '/test',
-                templateUrl: 'shopAdmin/views/test.html',
-                controller: 'testController'
-            });*/
     }
 ]).run(['$rootScope', '$state', 'Global', function ($rootScope, $state, Global) {
     $rootScope.$on('$stateChangeStart',
