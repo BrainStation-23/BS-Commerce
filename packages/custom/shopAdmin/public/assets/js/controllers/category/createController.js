b590f8f0adb06bc87893c75247b890c0305bee43
Zahid <Zahid@BrainStation-23.com>
2015-04-23 13:47:57 +0600

    Temporarily removed shop admin for conflict wit public page

--- packages/custom/shopAdmin/public/assets/js/controllers/category/createController.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('mean.shopAdmin').controller('categoryCreateController', ['$scope', 'Global',
-    function($scope, Global) {
-        //$scope.catId = $stateParams.catId;
-        //console.log($scope.catId);
-    }
-]);
--- packages/custom/shopAdmin/public/assets/js/controllers/category/editController.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('mean.shopAdmin').controller('categoryEditController', ['$scope', 'Global', '$stateParams',
-    function($scope, Global, $stateParams) {
-        $scope.catId = $stateParams.catId;
-        console.log($scope.catId);
-    }
-]);
