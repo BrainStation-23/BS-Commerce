c840eed91877f5534b912f683e1531733cb6d8af
Shaishab <shaishab.cse@gmail.com>
2015-05-25 20:22:05 +0600

    add controller to edit user and view add in previous commit

--- packages/custom/shopAdmin/public/assets/js/controllers/user/userEditController.js
+++ packages/custom/shopAdmin/public/assets/js/controllers/user/userEditController.js
@@ -3,17 +3,17 @@
  */
 'use strict';
 
-angular.module('mean.shopAdmin').controller('userEditController', ['$scope', 'Global', '$stateParams','customerService','$location',
-    function($scope, Global, $stateParms, customerService, $location) {
+angular.module('mean.shopAdmin').controller('userEditController', ['$scope', 'Global', '$stateParams','userService','$location',
+    function($scope, Global, $stateParms, userService, $location) {
         $scope.user = {};
         $scope.getUserById = function() {
             var customerId = $location.path().split('/')[3];
             //console.log(customerId);
-            var getCustomer = customerService.getCustomerById(customerId);
+            var getCustomer = userService.getUserById(customerId);
 
             getCustomer.$promise.then(function (user){
-                $scope.user = user[0];
-                console.log($scope.user);
+                $scope.user = user;
+                //console.log(user);
             });
         };
         $scope.getUserById();
