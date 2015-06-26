de31f25266a9f95a72a503d0dd51d1a07aaba7e9
Shaishab <shaishab.cse@gmail.com>
2015-06-26 19:58:38 +0600

    add shopCatalog route path and added related file

--- packages/custom/shopCatalog/server/routes/shopcatalog.js
+++ packages/custom/shopCatalog/server/routes/shopcatalog.js
@@ -6,7 +6,8 @@ var cartController = require('../controllers/cartController'),
 module.exports = function(Shopcatalog, app, auth, database, shopCore) {
   app.route('/api/cart')
     .get(cartController.getCart)
-    .put(cartController.update);
+    .put(cartController.update)
+    .delete(cartController.deleteCartById);
 
   app.route('/api/auth/order')
       .get(orderController.getOrders)
