521bf0692445edea233e87dd2942e8ebd381b46b
Shaishab <shaishab.cse@gmail.com>
2015-06-26 20:01:49 +0600

    added some route path and path related file added in previous commit

--- packages/custom/shopCatalog/public/routes/checkoutRoute.js
+++ packages/custom/shopCatalog/public/routes/checkoutRoute.js
@@ -1,19 +1,36 @@
 (function(){
-  'use strict';
+'use strict';
 
-  angular.module('mean.shopCatalog').config(['$stateProvider',
-	function($stateProvider) {
-	  $stateProvider.state('cart', {
-		url: '/cart',
-		controller: 'cartController',
-		templateUrl: 'shopCatalog/views/cart.html'
-	  });
+	angular.module('mean.shopCatalog').config(['$stateProvider',
+		function($stateProvider) {
+			$stateProvider.state('cart', {
+			url: '/cart',
+			controller: 'cartController',
+			templateUrl: 'shopCatalog/views/cart.html'
+			});
 
-	  $stateProvider.state('checkout', {
-		url: '/checkout',
-		controller: 'checkoutController',
-		templateUrl: 'shopCatalog/views/checkout.html'
-	  });
-	}
-  ]);
+			$stateProvider.state('checkout', {
+			url: '/checkout',
+			controller: 'checkoutController',
+			templateUrl: 'shopCatalog/views/checkout.html'
+			});
+
+			$stateProvider.state('checkout/complete', {
+				url: '/checkout/complete',
+				//controller: 'checkoutController',
+				templateUrl: 'shopCatalog/views/checkoutComplete.html'
+			});
+
+			$stateProvider.state('cart/empty', {
+				url: '/cart/empty',
+				templateUrl: 'shopCatalog/views/emptyCart.html'
+			});
+
+			$stateProvider.state('orderDetails/:orderId', {
+				url: '/orderDetails/:orderId',
+				//controller: 'orderController',
+				templateUrl: 'shopCatalog/views/orderDetails.html'
+			});
+		}
+	]);
 })();
