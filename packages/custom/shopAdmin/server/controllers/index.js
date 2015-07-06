38d4a0668b61d06676efef8b3a4f2e754cb5edeb
saiful <saiful@brainstation-23.com>
2015-07-06 20:12:09 +0600

    .. working on automatic scripting

--- packages/custom/shopAdmin/server/controllers/index.js
+++ packages/custom/shopAdmin/server/controllers/index.js
@@ -2,7 +2,7 @@
 
 var mean = require('meanio');
 
-exports.render = function(req, res){
+exports.render = function (req, res) {
     var modules = [];
     // Preparing angular modules list with dependencies
     for (var name in mean.modules) {
@@ -17,6 +17,10 @@ exports.render = function(req, res){
         return req.user && req.user.roles.indexOf('admin') !== -1;
     }
 
+    function getAdminScripts() {
+        return 'blah blah blah';
+    }
+
     // Send some basic starting info to the view
     res.render('index-admin', {
         user: req.user ? {
@@ -26,6 +30,7 @@ exports.render = function(req, res){
             profile: req.user.profile,
             roles: req.user.roles
         } : {},
+        adminScripts: getAdminScripts(),
         modules: modules,
         isAdmin: isAdmin,
         adminEnabled: isAdmin() && mean.moduleEnabled('mean-admin')
