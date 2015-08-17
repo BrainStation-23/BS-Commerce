6de21d9248bc7e682cf5ab798cc9a0df5b5ae05f
Shaishab <shaishab.cse@gmail.com>
2015-08-17 17:45:25 +0600

    update brand route of server side

--- packages/custom/shopCatalog/server/routes/brand.route.js
+++ packages/custom/shopCatalog/server/routes/brand.route.js
@@ -6,14 +6,10 @@ var brandController = require('../controllers/brand.controller');
 module.exports = function(shopCatalog, app, auth, database, shopCore) {
     app.route('/api/brands')
         .get(brandController.getBrands)
-        .post(brandController.createBrand);
+        .post(brandController.createBrand)
+        .put(brandController.updateBrand);
 
-    app.route('/api/brands/:id')
+    app.route('/api/brands/:brandId')
         .get(brandController.getBrandById)
-        .put(brandController.update)
         .delete(brandController.deleteBrandById);
-
-    app.route('/api/brands/count')
-        .get(brandController.getCount);
-
 };
