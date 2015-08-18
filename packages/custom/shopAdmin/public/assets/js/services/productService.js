63c623fee26330b422e7c47ede449de3b6180982
Shaishab <shaishab.cse@gmail.com>
2015-08-18 18:45:56 +0600

    Refactoring: add product service in admin panel to devide service function from product controller to perform product related operation

---
+++ packages/custom/shopAdmin/public/assets/js/services/productService.js
@@ -0,0 +1,44 @@
+'use strict';
+
+angular.module('mean.shopAdmin').factory('productService', ['$resource',
+    function($resource) {
+        return {
+            createProduct: function(product) {
+                var createProduct = $resource('/api/products', {}, {
+                    'create': {method: 'POST'}
+                });
+                return createProduct.create({product: product});
+            },
+            getProduct: function(query) {
+                var getProduct = $resource('/api/products', {}, {
+                    'get': {method: 'GET'}
+                });
+                return getProduct.get(query);
+            },
+            getProductById: function(id) {
+                var getProductById = $resource('/api/products/:id', {id: '@id'}, {
+                    'get': {method: 'GET'}
+                });
+                return getProductById.get({id: id});
+            },
+            updateProduct: function(product) {
+                var updateProduct = $resource('/api/products', {}, {
+                    'put': {method: 'PUT'}
+                });
+                return updateProduct.put(product);
+            },
+            deleteProduct: function(id) {
+                var deleteProduct = $resource('/api/products/:id', {id: '@id'}, {
+                    'delete': {method: 'DELETE'}
+                });
+                return deleteProduct.delete({id: id});
+            },
+            deleteProductPhoto: function(id) {
+                var deleteProductPhoto = $resource('/api/products/photos/:id', {id: '@id'}, {
+                    'delete': {method: 'DELETE'}
+                });
+                return deleteProductPhoto.delete({id: id});
+            }
+        };
+    }
+]);
