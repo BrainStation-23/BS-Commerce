045fb33c8cf8e59d90ff9753fac1b9514ef67c90
Shaishab <shaishab.cse@gmail.com>
2015-08-19 16:46:59 +0600

    update product info schema

--- packages/custom/shopCatalog/server/models/Product.js
+++ packages/custom/shopCatalog/server/models/Product.js
@@ -1,84 +1,104 @@
 'use strict';
 
 var mongoose = require('mongoose'),
-  Schema = mongoose.Schema;
+    Schema = mongoose.Schema;
 
 var ProductSchema = new Schema({
-  info: {
-    name: {
-      type: String,
-      required: true
+    info: {
+        name: {
+            type: String,
+            required: true
+        },
+        shortDescription: String,
+        fullDescription: String,
+        sku: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        price: {
+            type: Number,
+            default: 0
+        },
+        oldPrice: {
+            type: Number,
+            default: 0
+        },
+        cost: {
+            type: Number,
+            default: 0
+        },
+        showOnHomePage: {
+            type: Boolean,
+            default: false
+        },
+        includeInTopMenu: {
+            type: Boolean,
+            default: false
+        },
+        allowToSelectPageSize: {
+            type: Boolean,
+            default: false
+        },
+        published: {
+            type: Boolean,
+            default: false
+        },
+        displayOrder: {
+            type: Number,
+            default: 1
+        },
+        publishDate:{
+            type:Date,
+            default: new Date()
+        }
     },
-    shortDescription: String,
-    fullDescription: String,
-    sku: {
-      type: String,
-      required: true,
-      unique: true
+    meta:{
+        keywords:{
+            type: [String],
+            default: []
+        },
+        title:{
+            type: String,
+            default: ''
+        },
+        description:{
+            type: String,
+            default: ''
+        },
+        friendlyPageName:{
+            type: String,
+            required:true,
+            unique: true
+        }
     },
-    price: {
-      type: Number,
-      default: 0
-    },
-    oldPrice: {
-      type: Number,
-      default: 0
-    },
-    cost: {
-      type: Number,
-      default: 0
-    },
-    publishDate:{
-      type:Date,
-      default: new Date()
-    }
-  },
-  meta:{
-    keywords:{
-      type: [String],
-      default: []
-    },
-    title:{
-      type: String,
-      default: ''
-    },
-    description:{
-      type: String,
-      default: ''
-    },
-    friendlyPageName:{
-      type: String,
-      required:true,
-      unique: true
-    }
-  },
-  tags:[{
-    type: Schema.Types.ObjectId,
-    ref: 'ProductTag'
-  }],
-  photos:[{
-    type: Schema.Types.ObjectId,
-    ref: 'fs.files'
-  }],
-  brands:[{
-    brandId:{
-      type: Schema.Types.ObjectId
-    }
-  }],
-  categories:[{
-    categoryId:{
-      type: Schema.Types.ObjectId,
-      required: true
-    },
-    isFeatured:{
-      type: Boolean,
-      default: false
-    },
-    displayOrder:{
-      type: Number,
-      default: 0
-    }
-  }]
+    tags:[{
+        type: Schema.Types.ObjectId,
+        ref: 'ProductTag'
+    }],
+    photos:[{
+        type: Schema.Types.ObjectId,
+        ref: 'fs.files'
+    }],
+    brands:[{
+        brandId:{
+            type: Schema.Types.ObjectId
+        }
+    }],
+    categories:[{
+        categoryId:{
+            type: Schema.Types.ObjectId,
+            required: true
+        },
+        isFeatured:{
+            type: Boolean,
+            default: false
+        },
+        displayOrder:{
+            type: Number,
+            default: 0
+        }
+    }]
 });
 
 mongoose.model('Product', ProductSchema);
