89b9317a4979e7faa92f6b5afc3f7533ce0cabd2
Shaishab <shaishab.cse@gmail.com>
2015-08-25 16:56:43 +0600

    update shopProduct service for ui side

--- packages/custom/shopCatalog/public/services/shopProduct.js
+++ packages/custom/shopCatalog/public/services/shopProduct.js
@@ -1,13 +1,29 @@
 'use strict';
 
 angular.module('mean.shopCatalog').factory('ShopProduct', ['$resource',
-  function($resource) {
-    return $resource('api/products/:id',{
-      id:'@_id'
-    },{
-      update: {
-        method: 'PUT'
-      }
-    });
-  }
+    function($resource) {
+        return {
+            update: function(id) {
+                return $resource('api/products/:id',{
+                    id:'@_id'
+                },{
+                    update: {
+                        method: 'PUT'
+                    }
+                });
+            },
+            getProductById: function(id) {
+                var getProductById = $resource('api/products/:id', {id:'@_id'}, {
+                    'get': {method: 'GET'}
+                });
+                return getProductById.get({id: id});
+            },
+            getFeaturedProducts: function(query) {
+                var getFeaturedProducts = $resource('/api/productsByCondition', {}, {
+                    'get': {method: 'GET'}
+                });
+                return getFeaturedProducts.get(query);
+            }
+        };
+    }
 ]);
