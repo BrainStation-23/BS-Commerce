bc50112b0d3a973e2ff96b5599a548db7a6736a4
Shaishab <shaishab.cse@gmail.com>
2015-08-25 16:55:51 +0600

    update shopProductFeaturedItem directive and ui to show featured product in home page and related change in index.html and main.css

--- packages/custom/shopCatalog/public/directives/product/shopProductFeaturedItem.js
+++ packages/custom/shopCatalog/public/directives/product/shopProductFeaturedItem.js
@@ -1,16 +1,53 @@
 'use strict';
 
-angular.module('mean.shopCatalog').directive('shopProductFeaturedItem', ['Global', 'ShopCatalog',
-    function(Global, ShopCatalog) {
+angular.module('mean.shopCatalog').directive('shopProductFeaturedItem', ['Global', 'ShopCatalog', 'ShopProduct', 'cartService',
+    function(Global, ShopCatalog, ShopProduct, cartService) {
         return{
+            restrict: 'AE',
             replace: true,
-            scope:{
-                featuredItemSrc:'@',
-                cornerImage:'@'
-            },
             templateUrl: '/shopCatalog/views/product/shop-product-featured-item.html',
             link: function(scope, element, attrs){
 
+                var updateCartStatus = function(){
+                    cartService.getCart()
+                        .$promise
+                        .then(function(cart){
+                            _.forEach(scope.products, function(product){
+                                var indexInCart = _.findIndex(cart.items, function(item){
+                                    return item.product._id === product._id;
+                                });
+
+                                product.addedToCart = indexInCart >= 0;
+                            });
+                        });
+
+                };
+
+                scope.getFeaturedProducts = function() {
+                    var query = {
+                        isFeatured: true,
+                        numberOfDisplay: 20
+                    };
+                    ShopProduct.getFeaturedProducts(query)
+                        .$promise
+                        .then(function(promise) {
+                            scope.products = promise.products;
+                            updateCartStatus();
+                        });
+                };
+                scope.getFeaturedProducts();
+
+                scope.toggleCartStatus = function(product, event){
+                    event.preventDefault();
+                    product.addedToCart = !(product.addedToCart);
+
+                    if(product.addedToCart) {
+                        cartService.addToCart(product, 1);
+                    }else{
+                        cartService.removeFromCart(product);
+                    }
+
+                };
             }
         };
     }
