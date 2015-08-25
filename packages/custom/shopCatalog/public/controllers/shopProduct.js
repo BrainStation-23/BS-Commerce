abe3d0da6b7f4b4afe8d688cfb96103c8f4bcc03
Shaishab <shaishab.cse@gmail.com>
2015-08-25 16:57:51 +0600

    update shop product controller

--- packages/custom/shopCatalog/public/controllers/shopProduct.js
+++ packages/custom/shopCatalog/public/controllers/shopProduct.js
@@ -1,46 +1,47 @@
 'use strict';
 
-/* jshint -W098 */
 angular.module('mean.shopCatalog').controller('ShopProductController', ['$scope', '$state', 'Global', 'ShopProduct', 'cartService',
-  function($scope, $state, Global, ShopProduct, cartService) {
-    $scope.global = Global;
-    var sku = $state.params.sku;
+    function($scope, $state, Global, ShopProduct, cartService) {
+        $scope.global = Global;
+        var sku = $state.params.sku;
 
-    $scope.product = null;
-    $scope.quantity = 1;
-    $scope.slides = [];
+        $scope.product = null;
+        $scope.quantity = 1;
+        $scope.slides = [];
 
-    ShopProduct.get({id: sku}, function(product){
-      $scope.product = product;
+        ShopProduct.getProductById(sku)
+            .$promise
+            .then(function(product) {
+                console.log(product);
+                $scope.product = product;
+                $scope.slides = _.map(product.photos, function(photo){
+                    return {
+                        image: '/api/products/photos/' + photo
+                    };
+                });
+                cartService.getCart()
+                    .$promise
+                    .then(function(cart){
+                        var indexInCart = _.findIndex(cart.items, function(item){
+                            return item.product._id === product._id;
+                        });
 
-      $scope.slides = _.map(product.photos, function(photo){
-        return {
-          image: '/api/products/photos/' + photo
+                        product.addedToCart = indexInCart >= 0;
+                    });
+
+            });
+
+        $scope.toggleCartStatus = function(product, event){
+            event.preventDefault();
+
+            product.addedToCart = !(product.addedToCart);
+
+            if(product.addedToCart) {
+                cartService.addToCart(product, $scope.quantity);
+            }else{
+                cartService.removeFromCart(product);
+            }
+
         };
-      });
-
-      cartService.getCart()
-        .$promise
-        .then(function(cart){
-          var indexInCart = _.findIndex(cart.items, function(item){
-            return item.product._id === product._id;
-          });
-
-          product.addedToCart = indexInCart >= 0;
-        });
-    });
-
-    $scope.toggleCartStatus = function(product, event){
-      event.preventDefault();
-
-      product.addedToCart = !(product.addedToCart);
-
-      if(product.addedToCart) {
-        cartService.addToCart(product, $scope.quantity);
-      }else{
-        cartService.removeFromCart(product);
-      }
-
-    };
-  }
+    }
 ]);
