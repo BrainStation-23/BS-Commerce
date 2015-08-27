0ecb964bb78cf98d753d944344fb5ad60a801b32
Shaishab <shaishab.cse@gmail.com>
2015-08-27 20:42:55 +0600

    change mongodb connection

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,8 @@
 'use strict';
 
 module.exports = {
-  db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'https://bs-commerce.herokuapp.com') + '/mean-prod',
+  db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'bs-commerce:bs-commerce@ds035633.mongolab.com:35633/bs-commerce'),
+  //mongodb://<dbuser>:<dbpassword>@ds035633.mongolab.com:35633/bs-commerce
   /**
    * Database options that will be passed directly to mongoose.connect
    * Below are some examples.
