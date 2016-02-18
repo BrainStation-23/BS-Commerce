956142db732bb86d4c8488b8bc79a0f0dfaceb3f
Shaishab <shaishab.cse@gmail.com>
2016-02-18 15:45:53 +0600

    Update shop category accordion directive in Client side
    Update shop category accordion directive to show multilavel category for cotegory accordion.

--- packages/custom/shopCatalog/public/directives/category/shopCategoryAccordion.js
+++ packages/custom/shopCatalog/public/directives/category/shopCategoryAccordion.js
@@ -2,54 +2,64 @@
 
 angular.module('mean.shopCatalog').directive('shopCategoryAccordion', ['Global', '$rootScope', '$state', 'ShopCatalog',
     function(Global, $rootScope, $state, ShopCatalog) {
-      var highlightIfSelected = function(category, slug){
-        if(category.slug === slug){
-          category.isOpen = true;
-          return true;
-        }else {
-          var subCategorySelected = _.any(category.subCategories, function (sub) {
-            return highlightIfSelected(sub, slug);
-          });
 
-          if(subCategorySelected){
-            category.isOpen = true;
-          }else{
-            category.isOpen = false;
-          }
-          return subCategorySelected;
-        }
-      };
-
-      var processCategorySelection = function(categories, slug){
-        _.forEach(categories,function(category){
-          highlightIfSelected(category, slug);
-        });
-      };
-
-      return{
-          replace: true,
-          templateUrl: '/shopCatalog/views/category/shop-category-accordion.html',
-          link: function(scope, element, attrs){
-            scope.slug = $state.params.slug;
-            scope.categories = [];
-
-            ShopCatalog
-              .query()
-              .$promise
-              .then(function(list){
-                scope.categories = list;
-
-                if(scope.slug){
-                  processCategorySelection(list,scope.slug);
-                }
-              })
-              .catch(console.log);
-
-            $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
-              scope.slug = (toState.name === 'products-in-category') ? toParams.slug : '';
-              processCategorySelection(scope.categories, scope.slug);
+        var highlightIfSelected = function(category, slug){
+            if(category.slug === slug){
+                category.isOpen = true;
+                return true;
+            }
+            var subCategorySelected = _.any(category.subCategories, function (sub) {
+                return highlightIfSelected(sub, slug);
             });
-          }
-      };
+
+            if(subCategorySelected){
+                category.isOpen = true;
+            }else{
+                category.isOpen = false;
+            }
+            return subCategorySelected;
+        };
+
+        var processCategorySelection = function(categories, slug){
+            _.forEach(categories,function(category){
+                highlightIfSelected(category, slug);
+            });
+        };
+
+        return{
+            restrict: "AE",
+            templateUrl: '/shopCatalog/views/category/shop-category-accordion.html',
+            link: function(scope, element, attrs){
+                scope.slug = $state.params.slug;
+                scope.categories = [];
+
+                ShopCatalog
+                    .query()
+                    .$promise
+                    .then(function(list){
+                        scope.categories = list;
+
+                        if(scope.slug){
+                            processCategorySelection(list, scope.slug);
+                        }
+                    });
+
+                scope.toggleCategory = function(category) {
+                    category.isOpen = !category.isOpen;
+
+                    if(category.isOpen) {
+                        angular.element("#"+ category.slug).show();
+                    } else {
+                        angular.element("#"+ category.slug).hide();
+                    }
+                };
+
+                $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+                    console.log(toState,toParams);
+                    scope.slug = (toState.name === 'products-in-category') ? toParams.slug : '';
+                    processCategorySelection(scope.categories, scope.slug);
+                });
+            }
+        };
     }
 ]);
