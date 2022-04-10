ae4520fb94d63466dc2a47c8da92a84e3b09afc9
Anjarul Islam <anjarul.bs23@gmail.com>
2022-04-10 12:04:06 +0600

    added validation decorator for validating service functions using joi schema

---
+++ packages/headless/src/decorators/service.validator.ts
@@ -0,0 +1,32 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+import * as Joi from 'joi';
+import { ServiceErrorResponse } from 'src/helper/serviceResponse/service.response.interface';
+
+/**
+ * Factory decorator for validating service function arguments against a Joi Schema at run-time.
+ * Only the first argument is validated
+ */
+
+export function validateParam(schema: Joi.Schema, options: Joi.ValidationOptions = { abortEarly: false, allowUnknown: false }) {
+    return function validateSchema(
+        target: any,
+        propertyName: string,
+        descriptor: TypedPropertyDescriptor<any>
+    ) {
+        const method = descriptor.value;
+        descriptor.value = function (...args) {
+            const { error } = schema.validate(args[0], options);
+            if (error) {
+                const response: ServiceErrorResponse = { error: '', errors: {} };
+                error.details.forEach((err) => {
+                    response.error += err.message;
+                    response.errors[err.path[0]] = [err.message];
+                })
+                throw new HttpException(response, HttpStatus.BAD_REQUEST);
+            }
+            else {
+                return method.apply(this, args);
+            }
+        };
+    }
+}
