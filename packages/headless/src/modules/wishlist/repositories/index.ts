76e881f637da972a18b6438d194f2b317ed38865
ismail0946 <Ismail.hosen@brainstation-23.com>
2022-04-19 14:11:16 +0600

    Some Changes in wishlist repository and services

--- packages/headless/src/modules/wishlist/repositories/index.ts
+++ packages/headless/src/modules/wishlist/repositories/index.ts
@@ -9,66 +9,53 @@ export class WishListRepository {
   constructor(private readonly db: IWishListDatabase) { }
 
   async isExistWishlist(userId: string): Promise<WishList | null> {
-    const wishList = await this.db.findWishListByUserId(userId);
-    return wishList;
+    return await this.db.findWishListByUserId(userId);
   }
 
   async isExistItem(userId: string, productId: string): Promise<WishList | null> {
-    const wishList = await this.db.findItemByUserIdAndProductId(userId, productId,   );
-    return wishList;
+    return await this.db.findItemByUserIdAndProductId(userId, productId,   );
   }
 
   async incrementItem(userId: string, item: Item): Promise<WishList | null> {
-    const wishList = await this.db.findWishListAndIncrementItem(userId, item);
-    return wishList;
+    return await this.db.findWishListAndIncrementItem(userId, item);
   }
 
   async addItem(userId: string, item: Item): Promise<WishList | null> {
-    const wishList = await this.db.findWishListAndAddItem(userId, item);
-    return wishList;
+    return await this.db.findWishListAndAddItem(userId, item);
   }
 
   async createWishlist(userId: string, items: Item[],): Promise<WishList | null> {
-    const wishList = await this.db.NewWishListCreate(userId, items);
-    return wishList;
+    return await this.db.NewWishListCreate(userId, items);
   }
 
   async getWishlist(userId: string): Promise<WishList | null> {
-    const wishList = await this.db.getWishListByUserId(userId);
-    return wishList;
+    return await this.db.getWishListByUserId(userId);
   }
 
   async getWishlistById(wishlistId: string): Promise<WishList | null> {
-    const wishList = await this.db.getWishListByWishlistId(wishlistId);
-    return wishList;
+    return await this.db.getWishListByWishlistId(wishlistId);
   }
 
   async deleteWishlistById(wishlistId: string): Promise<WishList | null> {
-    const wishList = await this.db.deleteWishListByWishlistId(wishlistId);
-    return wishList;
+    return await this.db.deleteWishListByWishlistId(wishlistId);
   }
 
   async updateWishlistItem(userId: string, item: Item,): Promise<WishList | null> {
-    const wishList = await this.db.updateWishlistItem(userId, item);
-    return wishList;
+    return await this.db.updateWishlistItem(userId, item);
   }
 
   async deleteWishlistItem(userId: string, item: Item,): Promise<WishList | null> {
-    const wishList = await this.db.deleteWishlistItem(userId, item);
-    return wishList;
+    return await this.db.deleteWishlistItem(userId, item);
   }
   async deleteWishlistItemByProductId(userId: string, product: string,): Promise<WishList | null> {
-    const wishList = await this.db.deleteWishlistItemByProductId(userId, product);
-    return wishList;
+    return await this.db.deleteWishlistItemByProductId(userId, product);
   }
 
   async getWishlistWithoutPopulate(userId: string): Promise<WishList | null> {
-    const wishList = await this.db.getWishListWithoutPopulateByUserId(userId);
-    return wishList;
+    return await this.db.getWishListWithoutPopulateByUserId(userId);
   }
 
   async deleteAllWishlistItems(userId: string): Promise<WishList | null> {
-    const wishList = await this.db.deleteAllWishlistItemsByUserId(userId);
-    return wishList;
+    return await this.db.deleteAllWishlistItemsByUserId(userId);
   }
 }
