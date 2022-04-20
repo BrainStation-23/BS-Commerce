07944cd693abec089a7c8aaea87304f999f337d8
DipuBala <dipankar.bala@brainstation-23.com>
2022-04-20 16:34:00 +0600

    Remove the if condition that was used before to check null value

--- packages/headless/src/modules/cart/repositories/index.ts
+++ packages/headless/src/modules/cart/repositories/index.ts
@@ -7,80 +7,53 @@ export class CartRepository {
   constructor(private readonly db: ICartDatabase) {}
 
   async isExistCart(userId: string): Promise<Cart | null> {
-    const cart = await this.db.findCartByUserId(userId);
-    if (!cart) return null;
-    return cart;
+    return await this.db.findCartByUserId(userId);
   }
 
   async isExistItem(userId: string, productId: string): Promise<Cart | null> {
-    const cart = await this.db.findItemByUserIdAndProductId(userId, productId);
-    if (!cart) return null;
-    return cart;
+    return await this.db.findItemByUserIdAndProductId(userId, productId);
   }
 
   async addItem(userId: string, item: Item): Promise<Cart | null> {
-    const addItem = await this.db.addItemByUserId(userId, item);
-    if (!addItem) return null;
-    return addItem;
+    return await this.db.addItemByUserId(userId, item);
   }
 
   async incrementItem(userId: string, item: Item): Promise<Cart | null> {
-    const cart = await this.db.incrementItem(userId, item);
-    if (!cart) return null;
-    return cart;
+    return await this.db.incrementItem(userId, item);
   }
 
   async createCart(userId: string, items: Item[]): Promise<Cart | null> {
-    const newCart = await this.db.createCart(userId, items);
-    if (!newCart) return null;
-    return newCart;
+    return await this.db.createCart(userId, items);
   }
 
   async getCart(userId: string): Promise<Cart | null> {
-    const cart = await this.db.getCart(userId);
-    if (!cart) return null;
-    return cart;
+    return await this.db.getCart(userId);
   }
 
   async deleteCartById(cartId: string): Promise<Cart | null> {
-    const deletedCart = await this.db.deleteCartById(cartId);
-    if (!deletedCart) return null;
-    return deletedCart;
+    return await this.db.deleteCartById(cartId);
   }
 
   async updateCartItem(userId: string, item: Item): Promise<Cart | null> {
-    const updatedCart = await this.db.updateCartItem(userId, item);
-    if (!updatedCart) return null;
-    return updatedCart;
+    return await this.db.updateCartItem(userId, item);
   }
 
   async deleteCartItem(userId: string, item: Item): Promise<Cart | null> {
-    const deletedCart = await this.db.deleteCartItem(userId, item);
-    if (!deletedCart) return null;
-    return deletedCart;
+    return await this.db.deleteCartItem(userId, item);
   }
 
   async deleteCartItemByProductId(
     userId: string,
     productId: string,
   ): Promise<Cart | null> {
-    const deletedCart = await this.db.deleteCartItemByProductId(
-      userId,
-      productId,
-    );
-    if (!deletedCart) return null;
-    return deletedCart;
+    return await this.db.deleteCartItemByProductId(userId, productId);
   }
 
   async getItemsWithoutPopulate(userId: string): Promise<Cart | null> {
-    const cart = await this.db.getItemsWithoutPopulate(userId);
-    if (!cart) return null;
-    return cart;
+    return await this.db.getItemsWithoutPopulate(userId);
   }
 
   async deleteAllCartItems(userId: string): Promise<Cart | null> {
-    const cart = await this.db.deleteAllCartItems(userId);
-    if (!cart) return null;
-    return cart;
+    return await this.db.deleteAllCartItems(userId);
   }
 }
