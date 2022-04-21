645211d94b3b558abe553519b5a5f784b6359d76
ismail0946 <Ismail.hosen@brainstation-23.com>
2022-04-21 15:50:17 +0600

    some changes

--- packages/headless/src/app.module.ts
+++ packages/headless/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ResolveGraphqlModule } from './graphql.module.dependency';
+import { HelperModule } from './helper/helper.module';
 import { ResolveRestModule } from './rest.module.dependency';
 type API = 'GRAPHQL' | 'REST';
 
 @Module({
-  imports: ('GRAPHQL' as API) ? ResolveGraphqlModule() : ResolveRestModule()
+  imports: [
+    HelperModule,
+    ...(('GRAPHQL' as API) ? ResolveGraphqlModule() : ResolveRestModule())
+  ]
 })
 export class AppModule {}
--- packages/headless/src/rest.module.dependency.ts
+++ packages/headless/src/rest.module.dependency.ts
@@ -1,9 +1,7 @@
-import { HelperModule } from './helper/helper.module';
 import { WishListModule as WishListRestModule } from './modules/wishlist/wishlist.rest.module';
 
 export const ResolveRestModule = () => {
   return [
-    HelperModule,
     WishListRestModule
   ];
 };
