177495a039e9dccf0ac32b982747329c3f0bcf6c
mostafa-bs23 <mostafa.mahmud@brainstation-23.com>
2022-05-09 20:52:15 +0600

    BC-41 Fix: Added random UUID

--- packages/headless/src/database/mongodb/manufacturer/manufacturer.model.ts
+++ packages/headless/src/database/mongodb/manufacturer/manufacturer.model.ts
@@ -1,48 +1,55 @@
 import { model, Schema } from 'mongoose';
 import { Manufacturer } from 'src/entity/manufacturer';
+import { randomUUID } from 'crypto';
 
-const ManufacturerSchema = new Schema<Manufacturer>({
-    id: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    name: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String
-    },
-    picture: {
-        type: String
-    },
-    isPublished: {
-        type: Boolean,
-        default: false
-    },
-    displayOrder: {
-        type: Number
-    },
-    seo: {
-        metaKeyword: {
+const ManufacturerSchema = new Schema<Manufacturer>(
+    {
+        id: {
             type: String,
-            default: ''
+            unique: true,
+            default: () => randomUUID()
         },
-        metaDescription: {
+        name: {
             type: String,
-            default: ''
+            required: true
         },
-        metaTitle: {
-            type: String,
-            default: ''
+        description: {
+            type: String
         },
-        SEFN: {
-            type: String,
-            default: ''
-        }
+        picture: {
+            type: String
+        },
+        isPublished: {
+            type: Boolean,
+            default: false
+        },
+        displayOrder: {
+            type: Number
+        },
+        seo: {
+            metaKeyword: {
+                type: String,
+                default: ''
+            },
+            metaDescription: {
+                type: String,
+                default: ''
+            },
+            metaTitle: {
+                type: String,
+                default: ''
+            },
+            SEFN: {
+                type: String,
+                default: ''
+            }
+        },
+    },
+    {
+        timestamps: true,
+        versionKey: false
     }
-});
+);
 
 const ManufacturerModel = model<Manufacturer>('manufacturer', ManufacturerSchema);
 
--- packages/headless/src/entity/manufacturer.ts
+++ packages/headless/src/entity/manufacturer.ts
@@ -1,5 +1,5 @@
 export class Manufacturer {
-    id: string;
+    id?: string;
     name: string;
     description: string;
     picture: string;
