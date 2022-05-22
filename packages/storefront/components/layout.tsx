a17ef5dbaa2095111850d7404c91ba1d65dc9c4a
NafiAsib <nafi.asib@gmail.com>
2022-05-23 03:07:00 +0600

    feat(layout-tailwind setup)

--- packages/storefront/components/layout.tsx
+++ packages/storefront/components/layout.tsx
@@ -1,5 +1,6 @@
 import type { NextComponentType } from "next";
 import Footer from "./footer";
+import Header from "./header";
 import Navbar from "./navbar";
 import Viewport from "./viewport";
 
@@ -7,7 +8,8 @@ const Layout: NextComponentType = ({ children }) => {
   return (
     <>
       <Viewport />
-      <Navbar />
+      <Header />
+      {/* <Navbar /> */}
       <main>{children}</main>
       <Footer />
     </>
