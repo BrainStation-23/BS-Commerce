3b15878180ed2948ea9d54f280909963ee893245
AnikBS <anik.barua@brainstation-23.com>
2022-05-24 14:24:11 +0600

    add product design & implementation

--- packages/storefront/components/product.tsx
+++ packages/storefront/components/product.tsx
@@ -1,29 +1,7 @@
 import type { GetServerSideProps, NextPage } from "next";
-// import Image from "next/image";
 import { useSelector, useDispatch } from "react-redux";
 import Image from "next/image";
-import img from "../images/laptop2.jpg";
 import img1 from "../images/laptop3.jpg";
-// import { connect } from "react-redux";
-// import productPic from "../public/product.jpeg";
-
-// import { InferGetServerSidePropsType } from "next";
-
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-//     const res = await fetch(`http://localhost:3000/products`);
-//     const data = await res.json();
-//     // console.log(data)
-//     return {
-//         props: {
-//             products: data,
-//         }, // will be passed to the page component as props
-//     };
-// };
-
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-//   getProducts()
-// };
-
 import { getProducts } from "../redux/product.actions";
 import { useEffect } from "react";
 
@@ -57,68 +35,67 @@ const Product: NextPage = (props) => {
                                                 />
                                             </div>
 
-                                            <div className="mb-0.5">
-
-                                            <div className="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-6xl text-black font-semibold">
-                                                <div className="bg-white px-0.1 py-0.1 rounded-full">
-                                                    <svg
-                                                        xmlns="http://www.w3.org/2000/svg"
-                                                        className="mx-2.5 my-2.5 h-10 w-10 p-2 inline-block hover:bg-[#40A944] rounded-[50px] text-5xl"
-                                                        fill="none"
-                                                        viewBox="0 0 24 24"
-                                                        stroke="currentColor"
-                                                        strokeWidth={1.5}
-                                                    >
-                                                        <path
-                                                            strokeLinecap="round"
-                                                            strokeLinejoin="round"
-                                                            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                                                        />
-                                                    </svg>
-                                                    <svg
-                                                        xmlns="http://www.w3.org/2000/svg"
-                                                        className="mx-2.5 my-2.5 h-10 w-10 p-2 inline-block hover:bg-[#40A944] rounded-[50px] text-5xl"
-                                                        fill="none"
-                                                        viewBox="0 0 24 24"
-                                                        stroke="currentColor"
-                                                        strokeWidth={1.5}
-                                                    >
-                                                        <path
-                                                            strokeLinecap="round"
-                                                            strokeLinejoin="round"
-                                                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                                                        />
-                                                    </svg>
-                                                    <svg
-                                                        xmlns="http://www.w3.org/2000/svg"
-                                                        className="mx-2.5 my-2.5 h-10 w-10 p-2 inline-block hover:bg-[#40A944] rounded-[50px] text-5xl"
-                                                        fill="none"
-                                                        viewBox="0 0 24 24"
-                                                        stroke="currentColor"
-                                                        strokeWidth={1.5}
-                                                    >
-                                                        <path
-                                                            strokeLinecap="round"
-                                                            strokeLinejoin="round"
-                                                            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                                                        />
-                                                    </svg>
-                                                    <svg
-                                                        xmlns="http://www.w3.org/2000/svg"
-                                                        className="mx-2.5 my-2.5 h-10 w-10 p-2 inline-block hover:bg-[#40A944] rounded-[50px] text-5xl"
-                                                        fill="none"
-                                                        viewBox="0 0 24 24"
-                                                        stroke="currentColor"
-                                                        strokeWidth={1.5}
-                                                    >
-                                                        <path
-                                                            strokeLinecap="round"
-                                                            strokeLinejoin="round"
-                                                            d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
-                                                        />
-                                                    </svg>
+                                            <div className="my-2">
+                                                <div className="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-6xl text-black font-semibold">
+                                                    <div className="bg-white px-0.1 py-0.1 rounded-full">
+                                                        <svg
+                                                            xmlns="http://www.w3.org/2000/svg"
+                                                            className="mx-2.5 my-2.5 h-10 w-10 p-2 inline-block hover:bg-[#40A944] rounded-[50px] text-5xl"
+                                                            fill="none"
+                                                            viewBox="0 0 24 24"
+                                                            stroke="currentColor"
+                                                            strokeWidth={1.5}
+                                                        >
+                                                            <path
+                                                                strokeLinecap="round"
+                                                                strokeLinejoin="round"
+                                                                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                                                            />
+                                                        </svg>
+                                                        <svg
+                                                            xmlns="http://www.w3.org/2000/svg"
+                                                            className="mx-2.5 my-2.5 h-10 w-10 p-2 inline-block hover:bg-[#40A944] rounded-[50px] text-5xl"
+                                                            fill="none"
+                                                            viewBox="0 0 24 24"
+                                                            stroke="currentColor"
+                                                            strokeWidth={1.5}
+                                                        >
+                                                            <path
+                                                                strokeLinecap="round"
+                                                                strokeLinejoin="round"
+                                                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                                                            />
+                                                        </svg>
+                                                        <svg
+                                                            xmlns="http://www.w3.org/2000/svg"
+                                                            className="mx-2.5 my-2.5 h-10 w-10 p-2 inline-block hover:bg-[#40A944] rounded-[50px] text-5xl"
+                                                            fill="none"
+                                                            viewBox="0 0 24 24"
+                                                            stroke="currentColor"
+                                                            strokeWidth={1.5}
+                                                        >
+                                                            <path
+                                                                strokeLinecap="round"
+                                                                strokeLinejoin="round"
+                                                                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                                                            />
+                                                        </svg>
+                                                        <svg
+                                                            xmlns="http://www.w3.org/2000/svg"
+                                                            className="mx-2.5 my-2.5 h-10 w-10 p-2 inline-block hover:bg-[#40A944] rounded-[50px] text-5xl"
+                                                            fill="none"
+                                                            viewBox="0 0 24 24"
+                                                            stroke="currentColor"
+                                                            strokeWidth={1.5}
+                                                        >
+                                                            <path
+                                                                strokeLinecap="round"
+                                                                strokeLinejoin="round"
+                                                                d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+                                                            />
+                                                        </svg>
+                                                    </div>
                                                 </div>
-                                            </div>
                                             </div>
                                         </div>
 
