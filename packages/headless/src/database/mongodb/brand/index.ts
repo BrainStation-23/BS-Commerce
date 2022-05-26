be52274547e975bf788c213ed1c3d68d74f2e1ff
tahsinZilani <shamsul.tahsin@brainstation-23.com>
2022-05-26 20:54:48 +0600

    database functions created for mysql

--- packages/headless/src/database/mongodb/brand/index.ts
+++ packages/headless/src/database/mongodb/brand/index.ts
@@ -7,6 +7,10 @@ import { BrandModel } from './brand.model';
 @Injectable()
 export class BrandDatabase implements IBrandDatabase {
 
+    async getAllBrands(skip?: number, limit?: number): Promise<Brand[] | null> {
+        return await BrandModel.find({}).skip(skip).limit(limit).lean();
+    }
+
     async getBrandByName(brandName: string): Promise<Brand | null> {
         return await BrandModel.findOne({ "info.name": brandName }).lean();
     }
@@ -18,16 +22,13 @@ export class BrandDatabase implements IBrandDatabase {
     async addNewBrand(brand: Brand): Promise<Brand | null> {
         return await BrandModel.create(brand);
     }
-
+    
+    async updateBrandById(brandId: string, brand: Brand): Promise<Brand | null> {
+        return await BrandModel.findOneAndUpdate({ id : brandId}, brand, { new: true }).lean();
+    }
+    
     async deleteBrandById (brandId: string): Promise<Brand | null>{
         return await BrandModel.findOneAndRemove({ id : brandId }).lean();
     }
 
-    async getAllBrands(skip?: number, limit?: number): Promise<Brand[] | null> {
-        return await BrandModel.find({}).skip(skip).limit(limit).lean();
-    }
-
-    async updateBrandById(brandId: string, brand: Brand): Promise<Brand | null> {
-        return await BrandModel.findOneAndUpdate({ id : brandId}, brand, { new: true }).lean();
-    }
 }
