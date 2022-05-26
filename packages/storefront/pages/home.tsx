5505f6455116cca43a349e1f62b0e2b1134f3563
sakibbye <sadman.sakib0008@gmail.com>
2022-05-26 11:51:23 +0600

    Product details page

--- packages/storefront/pages/home.tsx
+++ packages/storefront/pages/home.tsx
@@ -1,8 +1,13 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import Image from "next/image";
+import Link from "next/link";
+import {useState} from "react";
 import productPic from "../public/product.jpeg";
+import { InferGetServerSidePropsType } from "next";
 
-const Home: NextPage = () => {
+const Home: NextPage = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const  [products, setProducts] = useState(props.products.data);
+  //console.log(products);
   return (
     <>
       <header className="bg-dark py-5">
@@ -18,7 +23,7 @@ const Home: NextPage = () => {
       <section className="py-5">
         <div className="container px-4 px-lg-5 mt-5">
           <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-            <div className="col mb-5">
+            {/* <div className="col mb-5">
               <div className="card h-100">
                 <Image className="card-img-top" src={productPic} alt="..." />
                 <div className="card-body p-4">
@@ -38,8 +43,8 @@ const Home: NextPage = () => {
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="col mb-5">
+            </div> */}
+            {/* <div className="col mb-5">
               <div className="card h-100">
                 <Image className="card-img-top" src={productPic} alt="..." />
                 <div className="card-body p-4">
@@ -59,10 +64,30 @@ const Home: NextPage = () => {
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="col mb-5">
+            </div> */}
+            { products.map((product : any) => (
+              <Link href={'/products/'+product.id} className="col mb-5" key={product.id}>
               <div className="card h-100">
-                <Image className="card-img-top" src={productPic} alt="..." />
+                <Image className="card-img-top" src={productPic} alt="..."  width="450" height="300"/>
+                <div className="card-body p-4">
+                  <div className="text-center">
+                    <h5 className="fw-bolder">{ product.name }</h5>
+                    { product.price }
+                  </div>
+                </div>
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <div className="text-center">
+                    {/* <a href="/productDetails"> */}
+                      View options
+                    {/* </a> */}
+                  </div>
+                </div>
+              </div>
+            </Link>
+            ))}
+            {/* <div className="col mb-5">
+              <div className="card h-100">
+                <Image className="card-img-top" src={productPic} alt="..."  />
                 <div className="card-body p-4">
                   <div className="text-center">
                     <h5 className="fw-bolder">Fancy Product</h5>
@@ -80,12 +105,23 @@ const Home: NextPage = () => {
                   </div>
                 </div>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       </section>
     </>
   );
 };
+
+export const getServerSideProps: GetServerSideProps = async(context) => {
+  const res = await fetch(`http://localhost:3000/products`);
+  const products : [] = await res.json()
+  // console.log(data)
+  return {
+    props:{
+      products
+    } 
+  }
+}
 
 export default Home;
