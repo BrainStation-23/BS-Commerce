eb72306c78a1efa38b99b89c53798a0763f9e1cb
AnikBS <anik.barua@brainstation-23.com>
2022-06-01 11:00:12 +0600

    deleted swiper

--- packages/storefront/pages/home.tsx
+++ packages/storefront/pages/home.tsx
@@ -1,12 +1,10 @@
 import type { NextPage } from "next";
-import Swaper from "./swaper";
 import Products from "./trendProducts";
 
 const Home: NextPage = () => {
   return (
     <>
       <Products></Products>
-      <Swaper></Swaper>
     </>
   );
 };
