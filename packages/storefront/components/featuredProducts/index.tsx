2fc3d08d82abca24443a77f8d5a2ffe6355f071e
SeefatHimel <Seefat.hossain@brainstation-23.com>
2022-06-02 13:19:39 +0600

    frontend/home backup

---
+++ packages/storefront/components/featuredProducts/index.tsx
@@ -0,0 +1,68 @@
+import type { NextComponentType } from "next";
+import Product from "./product";
+import productData from "../../allData/product-data.json";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Grid, Navigation, Pagination } from "swiper";
+
+import "swiper/css";
+import 'swiper/css/navigation';
+import "swiper/css/grid";
+import "swiper/css/pagination";
+
+const FeaturedProducts = () => {
+    return (
+        <div className="featuredProducts">
+            <div className="text-center mb-5  ">
+                <p className="text-sm font-light italic pt-5 md:text-sm lg:text-base ">Recently added our store</p>
+                <h1 className="font-bold mb-10 text-xl md:text-3xl lg:text-4xl ">Featured Products</h1>
+            </div>
+            <>
+                <Swiper
+                     spaceBetween={0}
+                     slidesPerView={3}
+                     modules={[Grid, Pagination,Navigation]}
+                     loop ={true}
+                     grid={{
+                         fill: 'row',
+                         rows: 3,
+                     }}
+                     
+                     breakpoints={{
+                        // when window width is >= 320px
+                        320: {
+                          slidesPerView: 1,
+                        //   spaceBetween: 20
+                        },
+                        // when window width is >= 480px
+                        480: {
+                          slidesPerView: 2,
+                        //   spaceBetween: 30
+                        },
+                        // when window width is >= 1024px
+                        1024: {
+                          slidesPerView: 3,
+                        //   spaceBetween: 30
+                        },
+                        // when window width is >= 2560px
+                        2560: {
+                          slidesPerView: 4,
+                        //   spaceBetween: 30
+                        }}}
+                     navigation= {true}
+                     className="mySwiper m-5"
+                >
+                    {productData["products"] &&
+                        productData["products"].length > 0 &&
+                        productData.products.map((product: any, index) => (                            
+                                <SwiperSlide key={product.id} className="p-4">
+                                    <Product product={product} />
+                                </SwiperSlide>                            
+                        ))}
+                    ...
+                </Swiper>
+            </>
+        </div>
+    );
+};
+
+export default FeaturedProducts;
---
+++ packages/storefront/components/featuredProducts/product.tsx
@@ -0,0 +1,66 @@
+import Icon from "../global/components/product/icon";
+import Picture from "../global/components/product/picture";
+import ProductInfo from "../global/components/product/productInfo";
+import { useEffect, useState } from "react";
+
+interface productInterface {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  images: [string];
+  category: string;
+}
+
+const Product = (props: { product: productInterface }) => {
+  const [pictureWidth, setPictureWidth] = useState<number>(92);
+  // const { product }: any = props;
+  const product = props.product;
+
+  function getResolution() {
+    console.log("Your screen resolution is: " + screen.width);
+    const sw = screen.width;
+
+    if (sw < 768) setPictureWidth(92);
+    else if (sw < 1024) setPictureWidth(108);
+    else if (sw <= 1440) setPictureWidth(110);
+    else setPictureWidth(120);
+  }
+
+  useEffect(() => {
+    getResolution();
+  }, [pictureWidth]);
+  return (
+    <>
+      <div className="col mb-0" key={product.id}>
+        <div className="transition duration-0 hover:duration-700 group py-3 hover:bg-white cursor-pointer">
+          <div className="rounded overflow-hidden max-w-sm">
+            <div className="relative">
+              <div
+                className={`group  max-w-xs grid grid-cols-3 gap-4 relative text-white overflow-hidden transition-all duration-700 `}
+              >
+                <div className="relative inset-0 bg-fill bg-center z-0">
+                  <Picture
+                    product={product}
+                    height={pictureWidth}
+                    width={pictureWidth}
+                    src={product.images[0]}
+                    alt={product.category}
+                  />
+                </div>
+                <div className="scale-0 group-hover:scale-100 transition-transform origin-left duration-300 absolute bottom-0.5 left-28 md:left-20 md:bottom-0 lg:bottom-1 lg:left-20 xl:left-32 xl:bottom-5 ">
+                  <Icon />
+                </div>
+                <div className="col-span-2 text-left">
+                  <ProductInfo product={product} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Product;
