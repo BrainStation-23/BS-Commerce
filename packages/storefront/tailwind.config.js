d458a69cdfa6c1c52a08bbbdab3beccda7cb369f
sbswarna <shatabdibiswasswarna@gmail.com>
2022-06-02 13:21:50 +0600

    Added screensize in tailwind.config.js.

--- packages/storefront/tailwind.config.js
+++ packages/storefront/tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        'sm': '320px',
+        'md': '495px',
+        'tab': '720px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px'
+      },
       keyframes: {
         'fade-in-down': {
             '0%': {
