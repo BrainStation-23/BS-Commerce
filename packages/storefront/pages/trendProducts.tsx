fc6f93eed67f677eb8032925357640229f8f36c8
AnikBS <anik.barua@brainstation-23.com>
2022-06-02 12:36:53 +0600

    added shipping file

---
+++ packages/storefront/pages/trendProducts.tsx
@@ -0,0 +1,13 @@
+import type { GetServerSideProps, NextPage } from "next";
+import Product from "../components/trend/products"
+
+const Products: NextPage = (props) => {
+
+    return (
+        <>
+            <Product></Product>
+        </>
+    );
+};
+
+export default Products;
