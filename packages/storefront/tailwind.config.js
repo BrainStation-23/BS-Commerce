9723aef0806074568e1d895aa854c7f13a389d90
asad <asad.hnk@gmail.com>
2022-06-03 12:15:03 +0600

    fix(remove custom screen size)

--- packages/storefront/tailwind.config.js
+++ packages/storefront/tailwind.config.js
@@ -5,14 +5,6 @@ module.exports = {
   ],
   theme: {
     extend: {
-      screens: {
-        'sm': '320px',
-        'md': '495px',
-        'tab': '720px',
-        'lg': '1024px',
-        'xl': '1280px',
-        '2xl': '1536px'
-      },
       keyframes: {
         'fade-in-down': {
             '0%': {
