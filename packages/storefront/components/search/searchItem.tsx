4308cbbe02e5b178b1581994db50ef6e96ad122e
sakibbye <sadman.sakib0008@gmail.com>
2022-06-03 12:18:48 +0600

    Searched products, product details page aligned for merge

--- packages/storefront/components/search/searchItem.tsx
+++ packages/storefront/components/search/searchItem.tsx
@@ -32,8 +32,6 @@ const SearchItem = () => {
 						tumeric sriracha taximy chia microdosing tilde DIY. XOXO
 						fam indxgo juiceramps cornhole raw denim forage
 						brooklyn. Everyday carry +1 seitan poutine tumeric.
-						Gastropub blue bottle austin listicle pour-over, neutra
-						jean shorts keytar banjo tattooed umami cardigan.
 					</p>
 
 					<div></div>
