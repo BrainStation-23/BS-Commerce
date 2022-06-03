b7f4e079a06e66ca73fd39722e1a62b70cf40bfe
sbswarna <shatabdibiswasswarna@gmail.com>
2022-06-03 15:52:21 +0600

    Fix(style & screensize agaim)

--- packages/storefront/components/global/breadcrumbs/breadcrumb.tsx
+++ packages/storefront/components/global/breadcrumbs/breadcrumb.tsx
@@ -11,9 +11,9 @@ const Breadcrumb: React.FC<Props> = (props) => {
   const { title, pathArray, linkArray } = props;
 
   return (
-    <div className="p-7 sm:p-5 md:p-12 lg:p-14 bg-[url('https://cdn.shopify.com/s/files/1/0359/6350/2651/files/banner18.jpg?v=1588133916')] bg-cover bg-no-repeat bg-center">
-      <h3 className="sm:text-2xl md:text-3xl lg:text-4xl text-center">{title}</h3>
-      <div className="flex flex-wrap items-center justify-center gap-2 sm:m-2 md:m-3 lg:m-3">
+    <div className="p-9 sm:p-5 md:p-12 lg:p-14 xl:p-14 bg-[url('https://cdn.shopify.com/s/files/1/0359/6350/2651/files/banner18.jpg?v=1588133916')] bg-cover bg-no-repeat bg-center">
+      <h3 className="text-2xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl text-center">{title}</h3>
+      <div className="flex flex-wrap items-center justify-center gap-2 m-2 sm:m-2 md:m-3 lg:m-3 xl:m-3">
         {pathArray.map((path, index) => {
           return (
             <React.Fragment key={index}>
