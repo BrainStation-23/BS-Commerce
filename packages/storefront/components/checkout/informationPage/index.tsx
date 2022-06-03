00083f5d9a1e052d868c41d80032016e81f0a9d6
sbswarna <shatabdibiswasswarna@gmail.com>
2022-06-03 16:09:30 +0600

    Fix(style & screensize again)

--- packages/storefront/components/checkout/informationPage/index.tsx
+++ packages/storefront/components/checkout/informationPage/index.tsx
@@ -4,7 +4,7 @@ import Information from "./information";
 const InformationPage = () => {
   return (
     <>
-      <div className="flex-initial lg:w-3/5">
+      <div className="flex-initial lg:w-3/5 xl:w-3/5">
         <div className="divide-y-2 mx-4 sm:mx-4 md:mx-20 lg:mx-20 xl:mx-20">
           <Information />
           <CheckoutFooter />
