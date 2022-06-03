526cb87d6c6d64066fb6d5a6dc7aabe4d26d76f6
shafi2t5 <Shafiul.Islam@brainstation-23.com>
2022-06-03 16:48:00 +0600

    feature home revamp

--- packages/storefront/pages/home.tsx
+++ packages/storefront/pages/home.tsx
@@ -2,11 +2,18 @@ import type { NextPage } from "next";
 import TrendingProducts from "@/components/trend/index";
 import WeekDeals from "@/components/weekDeals";
 import HomeShipping from "@/components/homeShipping";
-import HomeBanner from "@/components/homeBanner";
+// import HomeBanner from "@/components/homeBanner";
+import ImageSlider from "@/components/imageSlider";
+import FeaturedProducts from "@/components/featuredProducts";
+import BannerPage from "@/components/global/banners";
+import BestSell from "@/components/bestSell";
 
 const Home: NextPage = () => {
   return (
     <>
+      <div>
+        <ImageSlider />
+      </div>
       <div>
         <HomeShipping />
       </div>
@@ -14,10 +21,17 @@ const Home: NextPage = () => {
         <TrendingProducts />
       </div>
       <div className="mb-10">
-        <HomeBanner />
+        {/* <HomeBanner /> */}
+        <BannerPage />
+      </div>
+      <div className="mb-10">
+        <BestSell />
       </div>
       <div className="mb-10">
         <WeekDeals />
+      </div>
+      <div className="mb-10">
+        <FeaturedProducts />
       </div>
     </>
   );
