57777a6169efde453893f6d345cbaeca09fd32d6
DipuBala <dipankar.bala@brainstation-23.com>
2022-06-06 19:08:08 +0600

    test commit

--- packages/headless/src/database/mongodb/cart/index.ts
+++ packages/headless/src/database/mongodb/cart/index.ts
@@ -2,8 +2,8 @@ import { CartModel } from './cart.model';
 import { Injectable } from '@nestjs/common';
 import { Cart, Item } from 'src/entity/cart';
 import { ICartDatabase } from 'src/modules/cart/repositories/cart.database.interface';
-import { ProductModel } from '../product/product.model';
-import { Product } from 'src/entity/product';
+// import { ProductModel } from '../product/product.model';
+// import { Product } from 'src/entity/product';
 @Injectable()
 export class CartDatabase implements ICartDatabase {
 
@@ -21,19 +21,19 @@ export class CartDatabase implements ICartDatabase {
   }
 
   async getCartProduct(cart: Cart): Promise<any | null> {
-    const products = await ProductModel.find({
-      id: { $in: cart.items.map((item) => item.productId) },
-    }).select('info photos id -_id');
-    let map = new Map<string, Product>();
-    for (let i = 0, len = products.length; i < len; i++) {
-      map.set(products[i].id, products[i]);
-    }
-    return {
-      ...cart,
-      items: cart.items.map((item) => {
-        return { ...item, product: map.get(item.productId) };
-      }),
-    };
+    // const products = await ProductModel.find({
+    //   id: { $in: cart.items.map((item) => item.productId) },
+    // }).select('info photos id -_id');
+    // let map = new Map<string, Product>();
+    // for (let i = 0, len = products.length; i < len; i++) {
+    //   map.set(products[i].id, products[i]);
+    // }
+    // return {
+    //   ...cart,
+    //   items: cart.items.map((item) => {
+    //     return { ...item, product: map.get(item.productId) };
+    //   }),
+    // };
   }
 
   async isItemExist(userId: string, productId: string): Promise<Cart | null> {
