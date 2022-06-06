eac178adcf4032e0f30ec779fc4fd90d2a114683
sbswarna <shatabdibiswasswarna@gmail.com>
2022-06-06 14:39:04 +0600

    remove card icon bg

--- packages/storefront/components/global/icons-for-checkout-page/credit-card.tsx
+++ packages/storefront/components/global/icons-for-checkout-page/credit-card.tsx
@@ -3,8 +3,8 @@ const CreditCard = () => {
     <>
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        className="h-6 w-6 fill-yellow-500"
-        fill=""
+        className="h-6 w-6"
+        fill="none"
         viewBox="0 0 22 22"
         stroke="currentColor"
         strokeWidth="2"
