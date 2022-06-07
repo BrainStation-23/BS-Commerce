5ba22cfbbfa71f928dda189a3ff1cdcbc0dd1f6d
sbswarna <shatabdibiswasswarna@gmail.com>
2022-06-07 11:33:11 +0600

    remove unnecessary code

--- packages/storefront/components/checkout/shippingPage/shipping.tsx
+++ packages/storefront/components/checkout/shippingPage/shipping.tsx
@@ -13,12 +13,6 @@ const  Shipping = () => {
     return (
         <div className="w-full sm:w-full md:w-full lg:w-3/5 xl:w-3/5">
             <div className="px-3.5 m-0 md:mx-20 lg:mx-20 xl:mx-20 lg:my-12">
-                {/* <Path
-                    cart={path.cart}
-                    info={path.info}
-                    shipping={path.shipping}
-                    payment={path.payment}
-                ></Path> */}
                 <div className="box-border p-4 border-2 mt-5 divide-y-2 rounded-md text-center mx-auto">
                     <div className="flex justify-between items-center p-4">
                         <div className="flex flex-wrap gap-4">
