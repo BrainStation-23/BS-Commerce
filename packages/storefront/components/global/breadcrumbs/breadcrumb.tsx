5b8660dd3060815f61735fe93f582c4d35fdf6c3
sbswarna <shatabdibiswasswarna@gmail.com>
2022-06-07 11:12:33 +0600

    Fixed margin

--- packages/storefront/components/global/breadcrumbs/breadcrumb.tsx
+++ packages/storefront/components/global/breadcrumbs/breadcrumb.tsx
@@ -11,7 +11,7 @@ const Breadcrumb: React.FC<Props> = (props) => {
   const { title, pathArray, linkArray } = props;
 
   return (
-    <div className="p-9 sm:p-5 md:p-12 lg:p-14 xl:p-14 bg-[url('https://cdn.shopify.com/s/files/1/0359/6350/2651/files/banner18.jpg?v=1588133916')] bg-cover bg-no-repeat bg-center">
+    <div className="p-9 sm:p-5 md:p-12 lg:p-14 xl:p-14 bg-[url('https://cdn.shopify.com/s/files/1/0359/6350/2651/files/banner18.jpg?v=1588133916')] bg-cover bg-no-repeat bg-center mt-10">
       <h3 className="text-2xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl text-center">{title}</h3>
       <div className="flex flex-wrap items-center justify-center gap-2 m-2 sm:m-2 md:m-3 lg:m-3 xl:m-3">
         {pathArray.map((path, index) => {
