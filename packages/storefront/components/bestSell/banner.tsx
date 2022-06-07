44c49ae1eb0f8908aea51575a7d0da87f1eb912b
SeefatHimel <Seefat.hossain@brainstation-23.com>
2022-06-07 17:33:46 +0600

    Best sell bug fix

--- packages/storefront/components/bestSell/banner.tsx
+++ packages/storefront/components/bestSell/banner.tsx
@@ -9,8 +9,8 @@ const Banner = () => {
   return (
     <>
       <div className="m-auto h-96 mb-4 md:mb-0 md:h-full md:m-0 row-span-full flex flex-col-reverse bg-[url('https://cdn.shopify.com/s/files/1/0359/6350/2651/files/banner4_grande.jpg?v=1588052424')] bg-cover bg-no-repeat bg-center">
-        <div className="ml-10 mb-9">
-          <p className="sm:text-normal lg:text-5xl font-normal mb-2 w-1">
+        <div className="ml-5 mb-6 lg:mb-7">
+          <p className="text-4xl font-thin mb-2 w-1">
             {banner.title}
           </p>
           <ShopNowIcon />
