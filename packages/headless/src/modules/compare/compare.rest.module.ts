1b0c021aa76515104534423f03b73f3e3bccb305
bsdhrubo <diptonil.singho@brainstation-23.com>
2022-06-08 15:05:18 +0600

    Added test dto/controller/service

--- packages/headless/src/modules/compare/compare.rest.module.ts
+++ packages/headless/src/modules/compare/compare.rest.module.ts
@@ -3,12 +3,15 @@ import { ResolveDatabaseDependency } from 'src/database/database.resolver';
 import { CompareRepository } from './repositories';
 import { ICompareDatabase } from './repositories/compare.db.interface';
 import { CompareController } from './rest';
+import { CompareTestController } from './rest/test.controller';
 import { CompareService } from './services';
+import { CompareTestService } from './services/test.service';
 
 @Module({
-  controllers: [CompareController],
+  controllers: [CompareController, CompareTestController],
   providers: [
     CompareService,
+    CompareTestService,
     CompareRepository,
     {
       provide: ICompareDatabase,
