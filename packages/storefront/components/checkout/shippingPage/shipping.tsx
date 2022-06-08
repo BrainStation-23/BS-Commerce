cff6afdcf60a9258211f5ee99bd0597ff9718a90
sbswarna <shatabdibiswasswarna@gmail.com>
2022-06-08 17:07:23 +0600

    link added

--- packages/storefront/components/checkout/shippingPage/shipping.tsx
+++ packages/storefront/components/checkout/shippingPage/shipping.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Path from "@/components/global/components/path";
 import OrderList from "@/components/checkout/orderList";
 import CheckoutFooter from "../checkoutFooter";
-import { useRouter } from "next/router";
 
 const path = {
     cart: false,
@@ -12,10 +11,6 @@ const path = {
 };
 
 const Shipping = () => {
-    const router = useRouter();
-    function handleClick () {
-        router.push('/payment')
-    }
     return (
         <div className="w-full sm:w-full md:w-full lg:w-3/5 xl:w-3/5">
             <div className="px-3.5 m-0 md:mx-20 lg:mx-20 xl:mx-20 lg:my-12">
@@ -59,9 +54,7 @@ const Shipping = () => {
 
                 <div className="mt-5 lg:flex">
                     <div>
-                        <button onClick={() => {
-                            handleClick();
-                        }} className="bg-[#000000] text-white py-5 px-6 rounded-md w-full text-lg">
+                        <button className="bg-[#000000] text-white py-5 px-6 rounded-md w-full text-lg">
                             Continue to payment
                         </button>
                     </div>
