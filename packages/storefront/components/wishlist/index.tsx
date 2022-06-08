a2465e3f5a1eb23dc2a26c214534cd1aaf5feab4
sbswarna <shatabdibiswasswarna@gmail.com>
2022-06-08 13:15:36 +0600

    Change in design

--- packages/storefront/components/wishlist/index.tsx
+++ packages/storefront/components/wishlist/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import WishlistIcon from "./wishlist-icon";
 import productData from "../../allData/product-data.json";
 import Breadcrumb from "../global/breadcrumbs/breadcrumb";
+import Link from "next/link";
 
 const WishlistComponent = () => {
   function handleClick(data: any) {
@@ -18,13 +19,28 @@ const WishlistComponent = () => {
         {productData["products"].map((product, index) => {
           return (
             <React.Fragment key={index}>
-              <div className="flex flex-col items-center justify-center w-28 sm:w-28 md:w-44 lg:w-56 xl:w-56">
-                <img
-                  src={product.images[0]}
-                  className="h-24 w-24 sm:h-24 md:h-40 lg:h-48 xl:h-48 sm:w-24 md:w-40 lg:w-48 xl:w-48"
-                />
-                <p className="mt-2 text-center">{product.title}</p>
-                <p className="mt-2">{product.price}</p>
+              <div className="flex flex-wrap flex-col items-center">
+                <Link href="/product/1" passHref>
+                  <div className="cursor-pointer flex flex-col items-center justify-center w-28 sm:w-28 md:w-44 lg:w-56 xl:w-56">
+                    <img
+                      src={product.images[0]}
+                      className="h-24 w-24 sm:h-24 md:h-40 lg:h-48 xl:h-48 sm:w-24 md:w-40 lg:w-48 xl:w-48"
+                    />
+                    <p className="mt-2 text-center">{product.title}</p>
+                    <p className="mt-2 text-sm text-center">
+                      {(product.price -
+                        (product.price * product.discountPercentage) / 100.0).toFixed(3)}
+                      {product.price ? (
+                        <span className="text-xs font-semibold text-black ml-2">
+                          <s>$</s>
+                          {product.price ? <s>{product.price}</s> : null}
+                        </span>
+                      ) : (
+                        <div></div>
+                      )}
+                    </p>
+                  </div>
+                </Link>
                 <button
                   className="mb-5 mt-2 text-center"
                   onClick={() => {
