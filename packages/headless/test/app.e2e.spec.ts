f5f36c8d44daa70dd4147b26d4920d1baae31196
bsdhrubo <diptonil.singho@brainstation-23.com>
2022-06-09 19:03:26 +0600

    test running successfully

--- packages/headless/test/app.e2e.spec.ts
+++ packages/headless/test/app.e2e.spec.ts
@@ -1,65 +1,28 @@
 import { INestApplication } from '@nestjs/common';
-import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
-import { Connection } from 'mongoose';
 import { AppModule } from 'src/app.module';
-import { CompareModel } from 'src/database/mongodb/compare/compare.model';
-import {
-  AddToCompareDto,
-  CompareResponseDto,
-} from 'src/modules/compare/dto/compare.dto';
-import { CompareRepository } from 'src/modules/compare/repositories';
-import { CompareController } from 'src/modules/compare/rest';
-import { CompareService } from 'src/modules/compare/services';
+import { dbConfig } from 'src/config/database';
+import { connectToDatabase } from 'src/database/database.init';
+import { AddToCompareDto } from 'src/modules/compare/dto/compare.dto';
 import * as request from 'supertest';
 
 const token =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjZjYTNkY2E0LTcxNTctNDQwMC1iMDQ1LWE3OWZiOTg5NzcxNiIsInVzZXJuYW1lIjoidGVzdDFAbWFpbC5jb20iLCJsb2dJblRpbWUiOjE2NTQ3NjAyMDkzNTQsImlhdCI6MTY1NDc2MDIwOSwiZXhwIjoxNjU0ODQ2NjA5fQ.yzx5ePwTmWo4aDAZWp43IQng7RV3Edi5fOBLFjmxffs';
 
 const timeout = 30 * 1000;
-const userId = '76e9c15f-6816-439c-9a4f-e2fc902ffbf4';
 const productId = '76e9c15f-6816-439c-9a4f-e2fc902ffbf5';
 const compareDto: AddToCompareDto = { productId };
-const compareResDto: CompareResponseDto = {
-  items: [{ productId }],
-  userId,
-  id: '76e9c15f-6816-439c-9a4f-e2fc902ffbf3',
-};
-
-const mockCompareRepository = {
-  createCompare: jest.fn().mockResolvedValue(compareResDto),
-  addItemToCompare: jest.fn().mockImplementation((val) => console.log({ val })),
-  getCompareByUserId: jest.fn().mockResolvedValue(compareResDto),
-};
 
 describe('Initializing... (e2e)', () => {
   let app: INestApplication;
-  let service: CompareService;
-  let controller: CompareController;
-  let connection: Connection;
 
-  console.log(CompareModel.name);
+  beforeAll(async () => {
+    type DB = 'MONGO' | 'MYSQL';
+    await connectToDatabase(dbConfig.db as DB);
 
-  beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-      controllers: [CompareController],
-      providers: [
-        CompareService,
-        {
-          provide: CompareRepository,
-          useValue: mockCompareRepository,
-        },
-        {
-          provide: getModelToken('Compare'),
-          useValue: CompareModel,
-        },
-      ],
     }).compile();
-
-    service = module.get<CompareService>(CompareService);
-    controller = module.get<CompareController>(CompareController);
-
     app = module.createNestApplication();
     await app.init();
   });
@@ -68,20 +31,9 @@ describe('Initializing... (e2e)', () => {
     await app.close();
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-    expect(controller).toBeDefined();
+  afterAll(async () => {
+    await app.close();
   });
-
-  // it(
-  //   'add to compare',
-  //   async () => {
-  //     const result = await service.addItemToCompare(userId, productId);
-  //     console.log(result);
-  //     expect(result.code).toEqual(200);
-  //   },
-  //   timeout,
-  // );
 
   describe('GET /compare', () => {
     it('should return compare object', async () => {
