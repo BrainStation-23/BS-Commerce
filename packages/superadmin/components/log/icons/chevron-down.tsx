4ab2e7749b5df276917e8a0f067696de66d1203d
sbswarna <shatabdibiswasswarna@gmail.com>
2022-06-10 18:36:31 +0600

    scheduled task

--- packages/superadmin/components/log/icons/chevron-down.tsx
+++
@@ -1,9 +0,0 @@
-const ChevronDown = () => {
-    return (
-        <>
-            <i className="bi bi-chevron-down"></i>
-        </>
-    )
-}
-
-export default ChevronDown;
--- packages/superadmin/components/log/icons/chevron-up.tsx
+++
@@ -1,9 +0,0 @@
-const ChevronUp = () => {
-    return (
-        <>
-            <i className="bi bi-chevron-up"></i>
-        </>
-    )
-}
-
-export default ChevronUp;
