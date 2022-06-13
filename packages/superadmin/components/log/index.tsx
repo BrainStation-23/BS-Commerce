7059825fb8c286549901110475de76abc1344b08
sbswarna <shatabdibiswasswarna@gmail.com>
2022-06-13 16:27:06 +0600

    Modal Added

--- packages/superadmin/components/log/index.tsx
+++ packages/superadmin/components/log/index.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import LogHeading from "./heading-card";
 import LogSearch from "./log-search";
 import LogIndex from "./logTable";
+import Modal from "./modal";
 
 const LogPage = () => {
   return (
@@ -8,13 +10,25 @@ const LogPage = () => {
       <div className="d-flex flex-wrap justify-content-between mt-3">
         <h4>Log</h4>
         <div className="d-flex flex-wrap justify-content-end float-xs-right">
-          <button className="btn btn-danger me-1">
+          <button
+            type="button"
+            className="btn btn-danger me-1"
+            data-bs-toggle="modal"
+            data-bs-target="#myModal"
+          >
             <span className="me-2">
               <i className="bi bi-trash3"></i>
             </span>
             Delete Selected
           </button>
-          <button className="btn btn-danger">
+
+          <Modal />
+
+          <button
+            className="btn btn-danger"
+            data-bs-toggle="modal"
+            data-bs-target="#myModal"
+          >
             <span className="me-2">
               <i className="bi bi-trash3"></i>
             </span>
---
+++ packages/superadmin/components/log/modal.tsx
@@ -0,0 +1,43 @@
+const Modal = () => {
+  return (
+    <>
+     <div className="modal fade" id="myModal">
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h4 className="modal-title">Are you sure?</h4>
+                  <button
+                    type="button"
+                    className="btn-close"
+                    data-bs-dismiss="modal"
+                  ></button>
+                </div>
+
+                <div className="modal-body">
+                  Are you sure you want to perform this action?
+                </div>
+
+                <div className="modal-footer">
+                  <button
+                    type="button"
+                    className="btn btn-primary"
+                    data-bs-dismiss="modal"
+                  >
+                    Yes
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-secondary"
+                    data-bs-dismiss="modal"
+                  >
+                    No, cancel
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+    </>
+  );
+};
+
+export default Modal;
