4afee624d3f639aea802a7e52318b798f691030b
sbswarna <shatabdibiswasswarna@gmail.com>
2022-06-14 16:54:19 +0600

    Feat/report-customer-registered-customer

--- packages/superadmin/components/report/customer-report/registered-customer-report/index.tsx
+++ packages/superadmin/components/report/customer-report/registered-customer-report/index.tsx
@@ -1,13 +1,14 @@
 import RegisteredCustomer from "./registeredCustomer";
 
 const RegisteredCustomersReportComponent = () => {
-    return (
-        <>
-            <div>
-                <RegisteredCustomer />
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div>
+        <h4 className="py-3">Registered customers</h4>
+        <RegisteredCustomer />
+      </div>
+    </>
+  );
+};
 
 export default RegisteredCustomersReportComponent;
