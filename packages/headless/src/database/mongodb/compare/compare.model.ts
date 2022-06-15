ea02d273f85cbce0d8890d45c8aca159344eba02
bsdhrubo <diptonil.singho@brainstation-23.com>
2022-06-15 12:50:38 +0600

    Removed modelOptionsFactory

--- packages/headless/src/database/mongodb/compare/compare.model.ts
+++ packages/headless/src/database/mongodb/compare/compare.model.ts
@@ -1,37 +1,39 @@
 import { randomUUID } from 'crypto';
 import { model, Schema } from 'mongoose';
 import { Compare } from 'src/entity/compare';
-import { modelOptionsFactory } from '../common';
 
 const CompareItems = new Schema(
-  {
-    productId: {
-      type: String,
-    },
-  },
-  {
-    _id: false,
-    timestamps: false,
-    versionKey: false,
-  },
+	{
+		productId: {
+			type: String,
+		},
+	},
+	{
+		_id: false,
+		timestamps: false,
+		versionKey: false,
+	},
 );
 
 const CompareSchema = new Schema<Compare>(
-  {
-    id: {
-      type: String,
-      default: () => randomUUID(),
-      unique: true,
-    },
-    userId: {
-      type: String,
-      required: true,
-      unique: true,
-    },
+	{
+		id: {
+			type: String,
+			default: () => randomUUID(),
+			unique: true,
+		},
+		userId: {
+			type: String,
+			required: true,
+			unique: true,
+		},
 
-    items: [CompareItems],
-  },
-  modelOptionsFactory(),
+		items: [CompareItems],
+	},
+	{
+		timestamps: true,
+		versionKey: false,
+	},
 );
 
 const CompareModel = model<Compare>('compare', CompareSchema);
