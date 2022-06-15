0f9e10d8cd7bfeddeb4963af7258d5056aa20a62
sbswarna <shatabdibiswasswarna@gmail.com>
2022-06-15 14:23:08 +0600

    Fix(route)

--- packages/storefront/components/checkout/paymentPage/subsections/contactDetails.tsx
+++ packages/storefront/components/checkout/paymentPage/subsections/contactDetails.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 
-const ContactDetails = () => {
+const ContactDetails = (props: any) => {
+  const { setModal } = props;
   return (
     <>
       <div className="flex flex-wrap flex-col border rounded-lg px-5">
@@ -9,9 +10,20 @@ const ContactDetails = () => {
             <p className="text-gray-500">Contact</p>
             <p>shatabdi@gmail.com</p>
           </div>
-          <Link href="/information" passHref>
-            <a className="text-decoration-none text-xs">Change</a>
-          </Link>
+          <button
+              onClick={() => {
+                const obj = {
+                    info: true,
+                    ship: false,
+                    pay: false,
+                  }
+                setModal(obj);
+              }}
+              className="ml-10 text-sm"
+              style={{ border: "none" }}
+            >
+              Change
+            </button>
         </div>
         <hr />
         <div className="flex flex-wrap lg:items-center xl:items-center justify-between my-3 text-sm">
@@ -19,9 +31,20 @@ const ContactDetails = () => {
             <p className="text-gray-500">Ship to</p>
             <p>Dhaka, Bangladesh</p>
           </div>
-          <Link href="/information" passHref>
-            <a className="text-decoration-none text-xs">Change</a>
-          </Link>
+          <button
+              onClick={() => {
+                const obj = {
+                    info: true,
+                    ship: false,
+                    pay: false,
+                  }
+                setModal(obj);
+              }}
+              className="ml-10 text-sm"
+              style={{ border: "none" }}
+            >
+              Change
+            </button>
         </div>
         <hr />
         <div className="my-3 text-sm flex flex-wrap flex-col sm:flex-col md:flex-row lg:flex-row xl:flex-row gap-0 sm:gap-0 md:gap-6 lg:gap-6 xl:gap-6">
