b43f896a6f8fe6bffc98bd3c76593fc8bdf7138c
sbswarna <shatabdibiswasswarna@gmail.com>
2022-06-15 14:20:14 +0600

    Fix(route)

--- packages/storefront/components/checkout/shippingPage/index.tsx
+++ packages/storefront/components/checkout/shippingPage/index.tsx
@@ -22,6 +22,7 @@ const ShippingPage = (props: any) => {
                         info={path.info}
                         shipping={path.shipping}
                         payment={path.payment}
+                        setModal={setModal}
                     />
                 </div>
                 <div className="divide-x-0 sm:divide-x-0 md:divide-x-0 lg:divide-x-2 xl:divide-x-2 flex flex-wrap flex-col-reverse sm:flex-col-reverse md:flex-col-reverse lg:flex-row xl:flex-row justify-between">
@@ -32,6 +33,7 @@ const ShippingPage = (props: any) => {
                             info={path.info}
                             shipping={path.shipping}
                             payment={path.payment}
+                            setModal={setModal}
                         />
                     </div>
                     <OrderList />
--- packages/storefront/components/global/components/path.tsx
+++ packages/storefront/components/global/components/path.tsx
@@ -2,34 +2,71 @@ import React from "react";
 import Link from "next/link";
 
 interface Path {
-    cart: boolean;
-    info: boolean;
-    shipping: boolean;
-    payment: boolean;
+  cart: boolean;
+  info: boolean;
+  shipping: boolean;
+  payment: boolean;
+  setModal: Function;
 }
 
 // const Path: React.FC<Path> = ({cart, info, shipping, payment}) => {
 const Path = (props: Path) => {
-    const {cart, info, shipping, payment} = props
-    return (
-        <div className="flex flex-wrap text-xs gap-2">
-            <div className={cart ? "font-bold" : "font-normal"}>
-                <Link href="/cart">Cart</Link>
-            </div>
-            {" > "}
-            <div className={info ? "font-bold" : "font-normal"}>
-                <Link href="/checkout?step=information">Information</Link>
-            </div>
-            {" > "}
-            <div className={shipping ? "font-bold" : "font-normal"}>
-                <Link href="/checkout?step=shipping">Shipping</Link>
-            </div>
-            {" > "}
-            <div className={payment ? "font-bold" : "font-normal"}>
-                <Link href="/checkout?step=payment">Payment</Link>
-            </div>
-        </div>
-    );
+  const { cart, info, shipping, payment, setModal } = props;
+  return (
+    <div className="flex flex-wrap text-xs gap-2">
+      <div className={cart ? "font-bold" : "font-normal"}>
+        <Link href="/cart">Cart</Link>
+      </div>
+      {" > "}
+      <div className={info ? "font-bold" : "font-normal"}>
+        <button
+          onClick={() => {
+            const obj = {
+              info: true,
+              ship: false,
+              pay: false,
+            };
+            setModal(obj);
+          }}
+          style={{ border: "none" }}
+        >
+          Information
+        </button>
+      </div>
+      {" > "}
+      <div className={shipping ? "font-bold" : "font-normal"}>
+      <button
+          onClick={() => {
+            const obj = {
+              info: false,
+              ship: true,
+              pay: false,
+            };
+            setModal(obj);
+          }}
+          style={{ border: "none" }}
+        >
+          Shipping
+        </button>
+      </div>
+      {" > "}
+      <div className={payment ? "font-bold" : "font-normal"}>
+      <button
+          onClick={() => {
+            const obj = {
+              info: false,
+              ship: false,
+              pay: true,
+            };
+            setModal(obj);
+          }}
+          style={{ border: "none" }}
+        >
+          Payment
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Path;
