dcf7ad59a4bb392cd66fc71cde30a1598ddc346e
bsdhrubo <diptonil.singho@brainstation-23.com>
2022-06-21 19:12:54 +0600

    Updated utils successResponse

--- packages/headless/src/modules/compare/services/test.service.ts
+++ packages/headless/src/modules/compare/services/test.service.ts
@@ -5,32 +5,27 @@ import { CompareDataDto, CompareResponse } from '../dto/test.dto';
 import { CompareRepository } from '../repositories';
 @Injectable()
 export class CompareTestService {
-  constructor(private compareRepository: CompareRepository) {}
+    constructor(private compareRepository: CompareRepository) {}
 
-  async getCompareByUserId(
-    userId: string,
-  ): Promise<CompareResponse<CompareDataDto>> {
-    const data = (await this.compareRepository.getCompareByUserId(
-      userId,
-    )) as CompareDataDto;
-    if (data) {
-      const result = successResponse(data);
-      return result;
-    } else {
-      return errorResponse(GetCompareErrorEnum.COMPARISON_LIST_IS_EMPTY);
+    async getCompareByUserId(userId: string): Promise<CompareResponse<CompareDataDto>> {
+        const data = (await this.compareRepository.getCompareByUserId(userId)) as CompareDataDto;
+        if (data) {
+            const result = successResponse(CompareDataDto, data);
+            return result;
+        } else {
+            return errorResponse(GetCompareErrorEnum.COMPARISON_LIST_IS_EMPTY);
+        }
     }
-  }
 
-  async getCompare(userId: string): Promise<CompareResponse<boolean>> {
-    const data = (await this.compareRepository.getCompareByUserId(
-      userId,
-    )) as CompareDataDto;
-    if (data) {
-      const result = successResponse(true);
-      return result;
-    } else {
-      const err = errorResponse(GetCompareErrorEnum.COMPARISON_LIST_IS_EMPTY);
-      return err;
+    async getCompare(userId: string): Promise<CompareResponse<boolean>> {
+        const data = (await this.compareRepository.getCompareByUserId(userId)) as CompareDataDto;
+        if (data) {
+            // const result = successResponse(true);
+            // return result;
+            return null;
+        } else {
+            const err = errorResponse(GetCompareErrorEnum.COMPARISON_LIST_IS_EMPTY);
+            return err;
+        }
     }
-  }
 }
