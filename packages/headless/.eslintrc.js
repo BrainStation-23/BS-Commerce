fdd3f0926590dc7d5686ce09064f5736ca0e0a41
mostafa-bs23 <mostafa.mahmud@brainstation-23.com>
2022-06-21 17:53:29 +0600

    eslint fix

--- packages/headless/.eslintrc.js
+++ packages/headless/.eslintrc.js
@@ -3,11 +3,12 @@ module.exports = {
   parserOptions: {
     project: 'tsconfig.json',
     sourceType: 'module',
+    tsconfigRootDir: __dirname
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    // 'plugin:prettier/recommended',
   ],
   root: true,
   env: {
