af86e59573f78213e8cef77d6bcb860e9d44479c
bsdhrubo <diptonil.singho@brainstation-23.com>
2022-06-22 11:53:45 +0600

    Tab fixed compare model

--- packages/headless/src/database/mongodb/compare/compare.model.ts
+++ packages/headless/src/database/mongodb/compare/compare.model.ts
@@ -3,37 +3,37 @@ import { model, Schema } from 'mongoose';
 import { Compare } from 'src/entity/compare';
 
 const CompareItems = new Schema(
-	{
-		productId: {
-			type: String,
-		},
-	},
-	{
-		_id: false,
-		timestamps: false,
-		versionKey: false,
-	},
+  {
+    productId: {
+      type: String,
+    },
+  },
+  {
+    _id: false,
+    timestamps: false,
+    versionKey: false,
+  },
 );
 
 const CompareSchema = new Schema<Compare>(
-	{
-		id: {
-			type: String,
-			default: () => randomUUID(),
-			unique: true,
-		},
-		userId: {
-			type: String,
-			required: true,
-			unique: true,
-		},
+  {
+    id: {
+      type: String,
+      default: () => randomUUID(),
+      unique: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
 
-		items: [CompareItems],
-	},
-	{
-		timestamps: true,
-		versionKey: false,
-	},
+    items: [CompareItems],
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
 );
 
 const CompareModel = model<Compare>('compare', CompareSchema);
