6a8f9cc2c9b6e799241c76bee4b2ee290e427c04
sbswarna <shatabdibiswasswarna@gmail.com>
2022-06-22 16:31:01 +0600

    fix pid api call

--- packages/storefront/pages/product/[pid].tsx
+++ packages/storefront/pages/product/[pid].tsx
@@ -1,5 +1,6 @@
 import type { GetServerSideProps, NextPage } from "next";
 import ProductDetailsComponent from "@/components/product";
+import axios from "axios";
 import { userAPI } from "APIs";
 
 const ProductDetails: NextPage = () => {
@@ -11,12 +12,13 @@ const ProductDetails: NextPage = () => {
 };
 
 export async function getServerSideProps(context: any) {
-  const res = await userAPI.getPublicProductsById();
+  const { pid } = context.params;
+  const res = await userAPI.getPublicProductsById(pid);
   return {
     props: {
       products: res,
-    }
-  }
+    },
+  };
 }
 
 export default ProductDetails;
