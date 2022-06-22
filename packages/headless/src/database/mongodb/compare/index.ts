46bd16b0a0adcf7de892db2aeb0debe8e2cc1b4f
bsdhrubo <diptonil.singho@brainstation-23.com>
2022-06-22 13:19:15 +0600

    Tab fix

--- packages/headless/src/database/mongodb/compare/index.ts
+++ packages/headless/src/database/mongodb/compare/index.ts
@@ -3,60 +3,60 @@ import { ICompareDatabase } from 'src/modules/compare/repositories/compare.db.in
 import { CompareModel } from './compare.model';
 
 export class CompareDatabase implements ICompareDatabase {
-	async getCompareListByUserId(userId: string): Promise<Compare | null> {
-		const compareList = await CompareModel.findOne({
-			userId,
-		}).lean();
-		return compareList;
-	}
+  async getCompareListByUserId(userId: string): Promise<Compare | null> {
+    const compareList = await CompareModel.findOne({
+      userId,
+    }).lean();
+    return compareList;
+  }
 
-	async getCompareListById(userId: string, compareId: string): Promise<Compare | null> {
-		const compareList = await CompareModel.findOne({
-			id: compareId,
-			userId,
-		}).lean();
+  async getCompareListById(userId: string, compareId: string): Promise<Compare | null> {
+    const compareList = await CompareModel.findOne({
+      id: compareId,
+      userId,
+    }).lean();
 
-		return compareList;
-	}
+    return compareList;
+  }
 
-	async addItemToCompare(userId: string, productId: CompareItems): Promise<Compare> {
-		return await CompareModel.findOneAndUpdate(
-			{ userId: userId },
-			{ $addToSet: { items: productId } },
-			{ new: true },
-		).lean();
-	}
+  async addItemToCompare(userId: string, productId: CompareItems): Promise<Compare> {
+    return await CompareModel.findOneAndUpdate(
+      { userId: userId },
+      { $addToSet: { items: productId } },
+      { new: true },
+    ).lean();
+  }
 
-	async createCompare(userId: string, productId: CompareItems): Promise<Compare> {
-		return await CompareModel.create({
-			userId: userId,
-			items: [productId],
-		});
-	}
-	async deleteCompareById(userId: string, compareId: string): Promise<Compare> {
-		return await CompareModel.findOneAndRemove({
-			id: compareId,
-			userId,
-		}).lean();
-	}
+  async createCompare(userId: string, productId: CompareItems): Promise<Compare> {
+    return await CompareModel.create({
+      userId: userId,
+      items: [productId],
+    });
+  }
+  async deleteCompareById(userId: string, compareId: string): Promise<Compare> {
+    return await CompareModel.findOneAndRemove({
+      id: compareId,
+      userId,
+    }).lean();
+  }
 
-	async deleteItemByProductId(userId: string, productId: string): Promise<Compare> {
-		return await CompareModel.findOneAndUpdate(
-			{
-				userId: userId,
-			},
-			{ $pull: { items: { productId } } },
-			{ new: true },
-		).lean();
-	}
+  async deleteItemByProductId(userId: string, productId: string): Promise<Compare> {
+    return await CompareModel.findOneAndUpdate(
+      {
+        userId: userId,
+      },
+      { $pull: { items: { productId } } },
+      { new: true },
+    ).lean();
+  }
 
-	async deleteAllItemByUserId(userId: string): Promise<Compare> {
-		return await CompareModel.findOneAndUpdate(
-			{
-				userId: userId,
-			},
-			{ $set: { items: [] } },
-			{ new: true },
-		).lean();
-	}
+  async deleteAllItemByUserId(userId: string): Promise<Compare> {
+    return await CompareModel.findOneAndUpdate(
+      {
+        userId: userId,
+      },
+      { $set: { items: [] } },
+      { new: true },
+    ).lean();
+  }
 }
