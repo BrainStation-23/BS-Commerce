e8544b67018ae66d125539ba6018fd15631f9d80
SeefatHimel <Seefat.hossain@brainstation-23.com>
2022-06-23 18:55:30 +0600

    add modal

--- packages/superadmin/components/products/productsList.tsx
+++ packages/superadmin/components/products/productsList.tsx
@@ -15,19 +15,31 @@ const ProductsList: FC<Props> = ({ productsList, setProducts }) => {
   // const [checkAll, setCheckAll] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [PageSize, setPageSize] = useState(7);
+  const [ProductID, setProductID] = useState("");
 
   const onChangeForList = async (pageSize: number) => {
     const productsList = await userAPI.getProducts(pageSize);
     setProducts(productsList);
   };
 
-  const onClickForDelete = async (id: string) => {
-    const res = await userAPI.deleteProduct(id);
+  const deleteProductFunction = async () => {
+    const res = await userAPI.deleteProduct(ProductID);
     if (res) {
       onChangeForList(1000);
     }
+    setModal({
+      ...modal,
+      delete: false,
+    });
   };
 
+  const onClickForDelete = (id: string) => {
+    setProductID(id);
+    setModal({ ...modal, delete: true });
+  };
+  const [modal, setModal] = useState({
+    delete: false,
+  });
   const currentTableData = useMemo(() => {
     const firstPageIndex = (currentPage - 1) * PageSize;
     const lastPageIndex = firstPageIndex + PageSize;
@@ -185,6 +197,78 @@ const ProductsList: FC<Props> = ({ productsList, setProducts }) => {
           </div>
         </div>
       </div>
+      {modal.delete ? (
+        <div
+          className="modal"
+          style={{ display: modal.delete ? "block" : "none" }}
+        >
+          <div
+            className="modal-backdrop"
+            style={{
+              backgroundColor: "rgba(0, 0, 0, 0.1)",
+            }}
+            onClick={() => {
+              // close modal when outside of modal is clicked
+              setModal({ ...modal, delete: false });
+            }}
+          >
+            <div
+              className="modal-content"
+              onClick={(e) => {
+                // do not close modal if anything inside modal content is clicked
+                e.stopPropagation();
+              }}
+              style={{
+                textAlign: "left",
+                width: "30%",
+                marginLeft: "40%",
+                marginTop: "5%",
+                border: "1px solid gray",
+                boxShadow: "1px 1px 10px gray",
+                borderRadius: "10px",
+                padding: "20px",
+              }}
+            >
+              <div className="container">
+                <h1>Are you sure?</h1>
+                <hr />
+                <p>Are you sure you want to delete this item?</p>
+                <br />
+
+                <div className="clearfix">
+                  <button
+                    type="button"
+                    className="btn btn-light"
+                    style={{
+                      border: "1px solid gray",
+                      backgroundColor: "gray",
+                      color: "white",
+                      marginRight: "10px",
+                    }}
+                    onClick={() =>
+                      setModal({
+                        ...modal,
+                        delete: false,
+                      })
+                    }
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-danger"
+                    onClick={() => deleteProductFunction()}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div />
+      )}
     </>
   );
 };
