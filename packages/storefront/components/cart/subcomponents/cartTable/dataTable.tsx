a536fed26ee1dc6d451b0790b376f161b0e4d946
AnikBS <anik.barua@brainstation-23.com>
2022-06-24 15:31:20 +0600

    added cart

--- packages/storefront/components/cart/subcomponents/cartTable/dataTable.tsx
+++ packages/storefront/components/cart/subcomponents/cartTable/dataTable.tsx
@@ -2,31 +2,32 @@ import type { NextComponentType } from "next";
 import React, { useState } from "react";
 import Buttons from "../../../global/components/buttons/button";
 import Image from "next/image";
-import cartDatas from "../../../../allData/cart-data.json";
+// import cartDatas from "../../../../allData/cart-data.json";
 import { useAppSelector } from "customHooks/hooks";
 
 const DataTable = () => {
   const cartData = useAppSelector((state) => state.getAllCartItemsStore.allCartItems);
   console.log("From Data Table", cartData);
   const tableData = () => {
-    return cartDatas.data.items.map((cartData, index) => {
+    return cartData.items.map((cartData, index) => {
+      console.log("^^^^^^^^^^^^^^^^^^^^^^^^", cartData)
       return (
         <tr key={cartData.productId}>
           <td className="border border-slate-300 px-8 md:px-4 py-4">
-            <Image
-              src={cartData?.product.photos[0].url}
+            <img
+              src={cartData?.product?.photos[0]?.url}
               alt="product Image"
               width={100}
               height={90}
             />
           </td>
           <td className="border border-slate-300 md:px-2 xl:px-10 py-10">
-            {cartData.product.info.name}
+            {cartData?.product?.info?.name}
           </td>
           <td className="border border-slate-300 px-6 py-14 ">
             <span className="flex justify-center">
               {" "}
-              ${cartData.product.info.price}
+              ${cartData?.product?.info?.price}
             </span>
           </td>
           <td className="border border-slate-150 md:px-2 xl:px-10 py-4">
@@ -34,7 +35,7 @@ const DataTable = () => {
               <div className="box-content h-4 w-12 p-4 border-4">
                 <div className="flex justify-between">
                   <button>+</button>
-                  <div>{cartData.quantity}</div>
+                  <div>{cartData?.quantity}</div>
                   <button>-</button>
                 </div>
               </div>
