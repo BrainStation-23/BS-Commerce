794f38fbb65fa4f05ded83b63947e9747eb33b5c
tahsinZilani <shamsul.tahsin@brainstation-23.com>
2022-06-28 11:44:19 +0600

    testing done for brand-api

--- packages/models/src/common/errorResponse.ts
+++ packages/models/src/common/errorResponse.ts
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import { ErrorMessage } from "src/index";
-
-export interface ErrorResponse {
-    code?: number;
-    error: string;
-    errors: DescriptiveError | null;
-}
-
-export interface DescriptiveError {
-    [key: string]: [string];
-}
-
-
-=======
 export interface ErrorResponse {
     code?: number;
     error: string;
@@ -21,4 +6,3 @@ export interface ErrorResponse {
 export interface DescriptiveError {
     [key: string]: [string];
 }
->>>>>>> 3b26fc08e1c48e95b76ae149f09e58f18639426e
--- packages/models/src/common/successResponse.ts
+++ packages/models/src/common/successResponse.ts
@@ -1,8 +1,4 @@
 export interface SuccessResponse {
-<<<<<<< HEAD
-    code?: number;
-=======
     code: number;
     // data;
->>>>>>> 3b26fc08e1c48e95b76ae149f09e58f18639426e
 }
