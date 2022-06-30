28b683798bd0869e22dc0e965bb17949a8172265
sbswarna <shatabdibiswasswarna@gmail.com>
2022-06-30 15:39:35 +0600

    loader file modification

--- packages/storefront/components/global/loader.tsx
+++ packages/storefront/components/global/loader.tsx
@@ -10,17 +10,6 @@ const Loading = () => {
         linkArray={["/home", "/account/sign-in"]}
       />
       <div className="flex flex-wrap justify-center content-center my-20">
-        {/* <FadeLoader
-          color="#16A34A"
-          height={50}
-          margin={40}
-          speedMultiplier={1}
-          width={6}
-        />
-        <ClipLoader color="#16A34A" size={100} />
-
-        <HashLoader color="#16A34A" size={100} /> */}
-
         <BeatLoader color="#16A34A" margin={32} size={32} />
       </div>
     </>
