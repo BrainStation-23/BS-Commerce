ed7fd417cc8ee10e0bccfa64283463af984aca89
sbswarna <shatabdibiswasswarna@gmail.com>
2022-07-01 16:04:18 +0600

    fixed merging error

--- packages/storefront/components/layout.tsx
+++ packages/storefront/components/layout.tsx
@@ -8,22 +8,6 @@ import { useSelector } from "react-redux";
 import Axios from "axios";
 
 const Layout: NextComponentType = ({ children }: any) => {
-<<<<<<< HEAD
-    let token = useSelector((state: any) => state.persistedReducer.auth.access_token);
-    useEffect(() => {
-        Axios.defaults.headers.common = {
-            Authorization: `Bearer ${token}`,
-        };
-    }, [token]);
-    return (
-        <>
-            <Viewport />
-            <Header />
-            <main>{children}</main>
-            <Footer />
-        </>
-    );
-=======
   let token = useSelector(
     (state: any) => state.persistedReducer.auth.access_token
   );
@@ -40,7 +24,6 @@ const Layout: NextComponentType = ({ children }: any) => {
       <Footer />
     </>
   );
->>>>>>> b358c3a9b0285e138bc387e1166d6192754c7efa
 };
 
 export default Layout;
