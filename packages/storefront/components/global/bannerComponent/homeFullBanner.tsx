1e1c9412f9b94a0963d26a60fb854bac15b0abb4
sbswarna <shatabdibiswasswarna@gmail.com>
2022-07-13 15:48:30 +0600

    Fix banner

--- packages/storefront/components/global/bannerComponent/homeFullBanner.tsx
+++ packages/storefront/components/global/bannerComponent/homeFullBanner.tsx
@@ -9,12 +9,12 @@ const HomefullBanner: FC = () => {
       <Banner
         position="relative"
         width="w-full"
-        height="h-52 sm:h-52 md:h-52 lg:h-80 xl-80"
+        height="h-52 sm:h-52 md:h-52 lg:h-80 xl:h-80"
         hasButton={true}
         buttonText="Discover Now"
         linkhref="/deals" //need to modify href later
         bg="bg-[url('https://cdn.shopify.com/s/files/1/0359/6350/2651/files/banner3.jpg?v=1588050023')] bg-cover bg-no-repeat bg-center"
-        buttonPosition="absolute bottom-3 sm:bottom-3 md:bottom-3 lg:bottom-16 xl:bottom-12 left-0 md:left-4 lg:left-12"
+        buttonPosition="absolute bottom-3 sm:bottom-3 md:bottom-3 lg:bottom-16 xl:bottom-12 left-0 md:left-4 lg:left-12 xl:left-36 2xl:left-96"
         buttonEdge="rounded-lg"
         buttonPadding="px-3 sm:px-3 md:px-9 lg:px-9 xl:px-9 py-2 sm:py-2 lg:py-2.5 xl:py-2.5"
         buttonMargin="m-5"
