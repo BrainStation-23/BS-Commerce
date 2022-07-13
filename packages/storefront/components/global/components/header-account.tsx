5de72bfef1a5c7ba935441727ecd738d988e6647
AnikBS <anik.barua@brainstation-23.com>
2022-07-13 12:30:01 +0600

    fixed products show

--- packages/storefront/components/global/components/header-account.tsx
+++ packages/storefront/components/global/components/header-account.tsx
@@ -103,7 +103,7 @@ const HeaderAccount: React.FC<Properties> = () => {
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-6 w-6 cursor-pointer transition-all duration-100 ease-linear hover:text-green-600"
-              fill={wishlistItems.items?.length! > 0 && token !== '' ? `green` : `none`}
+              fill={wishlistItems?.items?.length! > 0 && token !== '' ? `green` : `none`}
               viewBox="0 0 24 24"
               stroke="currentColor"
               strokeWidth={2}
@@ -114,7 +114,7 @@ const HeaderAccount: React.FC<Properties> = () => {
                 d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
               />
             </svg>
-            <span>{wishlistItems.items?.length! > 0 && token !== '' ? wishlistItems.items?.length : 0}</span>
+            <span>{wishlistItems?.items?.length! > 0 && token !== '' ? wishlistItems.items?.length : 0}</span>
           </div>
         </button>
         <span className="z-50 text-sm" onClick={(e) => showCartDropDown()}>
