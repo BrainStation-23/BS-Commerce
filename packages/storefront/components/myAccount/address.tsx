e668bb1b0a5194e9f8d13d6708994475788cbee1
SeefatHimel <Seefat.hossain@brainstation-23.com>
2022-07-14 15:27:59 +0600

    c

--- packages/storefront/components/myAccount/address.tsx
+++ packages/storefront/components/myAccount/address.tsx
@@ -6,9 +6,15 @@ import { useState } from 'react';
 import AddNewAddressForm from './addNewAddressForm';
 
 const AccountDetails: NextComponentType = () => {
-  const [showAddress, setShowAddress] = useState('hidden');
-  const toggleButton = () => {
-    showAddress === '' ? setShowAddress('hidden') : setShowAddress('');
+  const [showAddAddress, setShowAddAddress] = useState('hidden');
+  const [showEditAddress, setShowEditAddress] = useState('hidden');
+  const addButtonOnClick = () => {
+    showAddAddress === '' ? setShowAddAddress('hidden') : setShowAddAddress('');
+  };
+  const editButtonOnClick = () => {
+    showEditAddress === ''
+      ? setShowEditAddress('hidden')
+      : setShowEditAddress('');
   };
   return (
     <>
@@ -18,22 +24,22 @@ const AccountDetails: NextComponentType = () => {
         linkArray={['/', '/myAccount/addresses']}
       />
       <div className="container mx-auto my-24 px-5 text-gray-800">
-        <div className='mb-3 flex flex-wrap justify-between'>
+        <div className="mb-3 flex flex-wrap justify-between">
           <div className="text-3xl font-bold">My Account</div>
           {/* <Link href="/myAccount/form" passHref> */}
-          <div className="text-xl mt-2 md:mt-0">
+          <div className="mt-2 text-xl md:mt-0">
             <button
-              className="rounded text-sm bg-green-600 py-2 text-white hover:bg-black focus:outline-none px-4"
+              className="rounded bg-green-600 py-2 px-4 text-sm text-white hover:bg-black focus:outline-none"
               type="button"
-              onClick={() => toggleButton()}
+              onClick={() => addButtonOnClick()}
             >
               Add a new address
             </button>
           </div>
           {/* </Link> */}
         </div>
-        <hr/>
-        <div className="mx-auto mt-5 grid grid-cols-1 md:grid-cols-3 gap-4">
+        <hr />
+        <div className="mx-auto mt-5 grid grid-cols-1 gap-4 md:grid-cols-3">
           <div className="md:col-span-1">
             <Link href="/myAccount">
               <span className="cursor-pointer hover:text-blue-600">
@@ -42,23 +48,33 @@ const AccountDetails: NextComponentType = () => {
             </Link>
           </div>
           <div className="md:col-span-2">
-            <div className={`${showAddress}`}><AddNewAddressForm/> <hr className='my-2' /></div>
-            <div className='text-center md:text-left'>
+            <div className={`${showAddAddress}`}>
+              <AddNewAddressForm /> <hr className="my-2" />
+            </div>
+            <div className="text-center md:text-left">
               <span className="text-4xl">Your Addresses</span>
               <div className="mt-2 text-3xl">Customer Name (Default)</div>
               <div className="mt-32 mb-5"> Bangladesh </div>
 
               <div>
-                <Link href="/account/addresses">
-                  <span className="cursor-pointer hover:text-blue-600">
-                    Edit{' '}
-                  </span>
-                </Link>
+                {/* <Link href="/account/addresses"> */}
+                <span
+                  className="cursor-pointer hover:text-blue-600"
+                  onClick={() => editButtonOnClick()}
+                >
+                  Edit{' '}
+                </span>
+                {/* </Link> */}
                 <span>| </span>
-                <span className="cursor-pointer hover:text-blue-600">Delete</span>
+                <span className="cursor-pointer hover:text-blue-600">
+                  Delete
+                </span>
               </div>
             </div>
-
+            <div className={`${showEditAddress}`}>
+              <hr className="my-2" />
+              <AddNewAddressForm /> <hr className="my-2" />
+            </div>
           </div>
         </div>
       </div>
