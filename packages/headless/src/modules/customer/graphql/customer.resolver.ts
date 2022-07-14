d4307cbca1ff2f102a249d633976059b547269d1
ismail0946 <Ismail.hosen@brainstation-23.com>
2022-07-14 14:16:40 +0600

    error remove

--- packages/headless/src/modules/customer/graphql/customer.resolver.ts
+++ packages/headless/src/modules/customer/graphql/customer.resolver.ts
@@ -1,28 +1,14 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { User } from 'src/entity/user';
 import { User as UserInfo } from 'src/decorators/auth.decorator';
-import { UserService } from '../services';
 import { UseGuards } from '@nestjs/common';
 import { ChangePasswordDto, UpdatedUserDto } from '../dto';
 import { RolesGuard } from 'src/guards/auth.guard';
+import { CustomerService } from '../services';
 
 @UseGuards(new RolesGuard(['customer']))
 @Resolver()
 export class CUstomerResolver {
-  constructor(private userService: UserService) { }
+  constructor(private userService: CustomerService) { }
 
-  @Query()
-  async getUser(@UserInfo() user: User) {
-    return await this.userService.getUser(user.id);
-  }
-
-  @Mutation()
-  async updateUser(@Args('data') data: UpdatedUserDto, @UserInfo() user: User) {
-    return await this.userService.updateUser(user.id, data);
-  }
-
-  @Mutation()
-  async changePassword(@Args('passwordDetails') passwordDetails: ChangePasswordDto, @UserInfo() user: User) {
-    return await this.userService.changePassword(user.id, passwordDetails);
-  }
 }
