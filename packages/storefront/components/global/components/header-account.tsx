24797c0c140050b97d5b44749daed8698c40ddda
NafiAsib <nafi.asib@gmail.com>
2022-07-14 13:49:45 +0600

    feat(storfront): My Profile page UI

--- packages/storefront/components/global/components/header-account.tsx
+++ packages/storefront/components/global/components/header-account.tsx
@@ -68,11 +68,28 @@ const HeaderAccount: React.FC<Properties> = () => {
         <span className="my-0 uppercase">
           {token !== '' ? (
             <div className="flex flex-wrap gap-2">
-              <Link href={links[4].link}>
-                <a className="cursor-pointer normal-case transition-all duration-100 ease-linear hover:text-green-600">
-                  {links[4].name}
-                </a>
-              </Link>
+              <div className="group relative cursor-pointer normal-case">
+                {links[4].name}
+                <div
+                  className={`absolute -left-[20px] top-[20px] z-50 hidden overflow-hidden whitespace-nowrap bg-white px-6 py-6 shadow-lg transition-all duration-300 ease-in group-hover:inline-block`}
+                >
+                  <ul>
+                    <Link href="/myAccount" passHref>
+                      <li className="transition-all duration-100 ease-linear hover:text-green-600">
+                        My Profile
+                      </li>
+                    </Link>
+
+                    <li className="transition-all duration-100 ease-linear hover:text-green-600">
+                      Orders
+                    </li>
+                    <li className="transition-all duration-100 ease-linear hover:text-green-600">
+                      Manage Addresses
+                    </li>
+                  </ul>
+                </div>
+              </div>
+
               <Link href={links[3].link}>
                 <a
                   onClick={() => setModalOn(true)}
@@ -98,12 +115,19 @@ const HeaderAccount: React.FC<Properties> = () => {
             </>
           )}
         </span>
-        <button onClick={handleClickWishlist} className="rounded-md border border-gray-300 px-4 py-2">
+        <button
+          onClick={handleClickWishlist}
+          className="rounded-md border border-gray-300 px-4 py-2"
+        >
           <div className="flex flex-wrap">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-6 w-6 cursor-pointer transition-all duration-100 ease-linear hover:text-green-600"
-              fill={wishlistItems?.items?.length! > 0 && token !== '' ? `green` : `none`}
+              fill={
+                wishlistItems?.items?.length! > 0 && token !== ''
+                  ? `green`
+                  : `none`
+              }
               viewBox="0 0 24 24"
               stroke="currentColor"
               strokeWidth={2}
@@ -114,28 +138,32 @@ const HeaderAccount: React.FC<Properties> = () => {
                 d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
               />
             </svg>
-            <span>{wishlistItems?.items?.length! > 0 && token !== '' ? wishlistItems.items?.length : 0}</span>
+            <span>
+              {wishlistItems?.items?.length! > 0 && token !== ''
+                ? wishlistItems.items?.length
+                : 0}
+            </span>
           </div>
         </button>
         <span className="z-50 text-sm" onClick={(e) => showCartDropDown()}>
           <CartDropdown />
         </span>
         <Link href="/order" passHref>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6 cursor-pointer transition-all duration-100 ease-linear hover:text-green-600"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth={2}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
-          />
-        </svg>
-      </Link>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 cursor-pointer transition-all duration-100 ease-linear hover:text-green-600"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+            />
+          </svg>
+        </Link>
       </div>
     </>
   );
--- packages/storefront/components/myAccount/account-details.tsx
+++ packages/storefront/components/myAccount/account-details.tsx
@@ -1,59 +1,178 @@
 import { NextComponentType } from 'next';
 import Link from 'next/link';
-
-import Breadcrumb from '@/components/global/breadcrumbs/breadcrumb';
+import { toast } from 'react-toastify';
 
 import { useAppDispatch, useAppSelector } from 'customHooks/hooks';
 import { storeUserToken } from 'toolkit/authSlice';
-import { toast } from 'react-toastify';
+
+import Breadcrumb from '@/components/global/breadcrumbs/breadcrumb';
+import { Field, Form, Formik } from 'formik';
+import { useState } from 'react';
 
 const AccountDetails: NextComponentType = () => {
+  const [editable, setEditable] = useState<boolean>(false);
   const dispatch = useAppDispatch();
-//   const token = useAppSelector(
-//     (state) => state.persistedReducer.auth.access_token
-//   );
+  //   const token = useAppSelector(
+  //     (state) => state.persistedReducer.auth.access_token
+  //   );
+
+  const userData = {
+    name: 'Test User',
+    email: 'test@user.com',
+    firstName: 'Test',
+    lastName: 'User',
+    phone: '+8801674314359',
+    isPhoneVerified: false,
+    isEmailVerified: true,
+  };
+
   const handleLogout = () => {
     localStorage.clear();
     dispatch(storeUserToken(''));
     toast.success('Logged out successfully!');
   };
+
+  const handleSubmit = (values: any) => {
+    console.log('clicked on save');
+
+    console.log(values);
+  };
+
   return (
     <>
-     <Breadcrumb
-        title='Account'
+      <Breadcrumb
+        title="Account"
         pathArray={['Home', 'Account']}
         linkArray={['/', '/myAccount']}
       />
-    <div className="container mx-auto my-24 px-5 text-gray-800">
-      <div className="border-b-2">
-        <span className="text-3xl font-bold">My Account</span>
-        <Link href="/" passHref>
-          <span className="float-right text-xl font-bold">
-            <a
-              onClick={() => handleLogout()}
-              className="cursor-pointer transition-all duration-100 ease-linear hover:text-green-600"
-            >
-              Logout
-            </a>
-          </span>
-        </Link>
-      </div>
-      <div className="mx-auto mt-3 flex flex-wrap">
-          <div className="w-full md:w-2/3">
-            <span className="text-2xl font-semibold">Order History</span>
-            <div className="mt-2">You haven't placed any orders yet.</div>
-          </div>
-          <div className="w-full md:w-1/3">
-            <span className="text-2xl font-semibold">Account Details</span>
-            <div className="mt-2 text-xl font-semibold">Customer Name</div>
-            <div className="mt-32"> Bangladesh </div>
+      <div className="container mx-auto my-24 px-5 text-gray-800">
+        <div className="border-b-2">
+          <span className="text-3xl font-bold">My Account</span>
+          <Link href="/" passHref>
+            <span className="float-right text-xl font-bold">
+              <a
+                onClick={() => handleLogout()}
+                className="cursor-pointer transition-all duration-100 ease-linear hover:text-green-600"
+              >
+                Logout
+              </a>
+            </span>
+          </Link>
+        </div>
+
+        <Formik
+          initialValues={{
+            firstName: userData.firstName,
+            lastName: userData.lastName,
+            phone: userData.phone,
+            email: userData.email,
+          }}
+          onSubmit={(values, actions) => {
+            handleSubmit(values);
+            actions.setSubmitting(false);
+          }}
+        >
+          {(formikProps) => {
+            return (
+              <Form onSubmit={formikProps.handleSubmit} className="">
+                <fieldset disabled={!editable}>
+                  <div className="mx-auto mt-3 flex flex-wrap">
+                    <div className="w-full md:w-1/3">
+                      <span className="">
+                        <span className="text-2xl font-semibold">
+                          Account Details
+                        </span>
+                        <span
+                          className="ml-2 cursor-pointer rounded-md bg-green-600 px-2 py-1 text-white transition-all duration-150 ease-linear hover:bg-stone-900"
+                          hidden={editable}
+                          onClick={() => setEditable(true)}
+                        >
+                          Edit
+                        </span>
+                        <button
+                          hidden={!editable}
+                          type="submit"
+                          className="ml-2 rounded-md bg-green-600 px-2 py-1 text-white transition-all duration-150 ease-linear hover:bg-stone-900"
+                        >
+                          Save
+                        </button>
+                        <span
+                          hidden={!editable}
+                          className="ml-2 cursor-pointer rounded-md bg-green-600 px-2 py-1 text-white transition-all duration-150 ease-linear hover:bg-stone-900"
+                          onClick={() => setEditable(false)}
+                        >
+                          Cancel
+                        </span>
+                      </span>
+                      {/* Account Form */}
+                      <div className="mt-2 flex flex-col gap-y-2">
+                        <div className="flex flex-row">
+                          <span className="mr-2 font-semibold">
+                            First name:{' '}
+                          </span>
+                          <Field
+                            className=""
+                            id="firstName"
+                            name="firstName"
+                            type="text"
+                          />
+                        </div>
+                        <div className="flex flex-row">
+                          <span className="mr-2 font-semibold">
+                            Last name:{' '}
+                          </span>
+                          <Field
+                            className=""
+                            id="lastName"
+                            name="lastName"
+                            type="text"
+                          />
+                        </div>
+                        <div className="flex flex-row">
+                          <span className="mr-2 font-semibold">Phone: </span>
+                          <Field
+                            className=""
+                            id="phone"
+                            name="phone"
+                            type="text"
+                            disabled={userData.isPhoneVerified}
+                            //
+                          />
+                          {editable
+                            ? ''
+                            : userData.isPhoneVerified
+                            ? ''
+                            : '(unverified)'}
+                        </div>
+                        <div className="flex flex-row">
+                          <span className="mr-2 font-semibold">Email: </span>
+                          <Field
+                            className=""
+                            id="email"
+                            name="email"
+                            type="email"
+                            disabled={userData.isEmailVerified}
+                          />
+                          {editable
+                            ? ''
+                            : userData.isEmailVerified
+                            ? ''
+                            : '(unverified)'}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </fieldset>
+              </Form>
+            );
+          }}
+        </Formik>
+        {/* <div className="mt-32"> Bangladesh </div>
             <Link href="/myAccount/addresses">
               <span className="cursor-pointer hover:text-green-600">
                 View Addresses (1)
               </span>
-            </Link>
-          </div>
-        </div>
+            </Link> */}
       </div>
     </>
   );
