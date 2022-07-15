6889eee38c327bb60b35b22e4c0c61c253dc4e66
sbswarna <shatabdibiswasswarna@gmail.com>
2022-07-15 17:42:57 +0600

    fix

--- packages/storefront/components/global/components/header-account.tsx
+++ packages/storefront/components/global/components/header-account.tsx
@@ -71,7 +71,7 @@ const HeaderAccount: React.FC<Properties> = () => {
               <div className="group relative cursor-pointer normal-case">
                 <p className='hover:text-green-600'>{links[4].name}</p>
                 <div
-                  className={`absolute -left-[20px] mt-2 top-[20px] z-50 hidden overflow-hidden whitespace-nowrap bg-white px-6 py-6 shadow-lg transition-all duration-300 ease-in group-hover:inline-block`}
+                  className={`absolute -left-[20px] top-[20px] z-50 hidden overflow-hidden whitespace-nowrap bg-white px-6 py-6 shadow-lg transition-all duration-300 ease-in group-hover:inline-block`}
                 >
                   <ul>
                     <Link href="/myAccount" passHref>
