74f9ece1e1dbce7648a326205cfd58e3b0df1fe0
sbswarna <shatabdibiswasswarna@gmail.com>
2022-07-15 18:32:22 +0600

    table width fix

--- packages/storefront/components/cart/subcomponents/cartTable/dataTable.tsx
+++ packages/storefront/components/cart/subcomponents/cartTable/dataTable.tsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 
-import { useRouter } from "next/router";
-import type { NextComponentType } from "next";
+import { useRouter } from 'next/router';
+import type { NextComponentType } from 'next';
 
-import { deleteCart } from "toolkit/cartSlice";
-import { useAppDispatch } from "customHooks/hooks";
+import { deleteCart } from 'toolkit/cartSlice';
+import { useAppDispatch } from 'customHooks/hooks';
 
-import TableData from "@/components/cart/subcomponents/cartTable/tableData";
+import TableData from '@/components/cart/subcomponents/cartTable/tableData';
 
 const DataTable: NextComponentType = () => {
   const dispatch = useAppDispatch();
@@ -14,72 +14,72 @@ const DataTable: NextComponentType = () => {
 
   return (
     <>
-      <div className="md:px-2 lg:px-20 xl:px-30 2xl:px-40 py-20">
-        <table className="border-collapse border border-slate-400">
-          <thead className="">
-            <tr>
-              <th className="border border-slate-300 px-16 py-4 md:px-8 text-base bg-slate-200">
-                Image
-              </th>
-              <th className="border border-slate-300 md:px-2 xl:px-10 py-4 text-base bg-slate-200">
-                Product
-              </th>
-              <th className="border border-slate-300 px-10 py-4 text-base bg-slate-200">
-                Price
-              </th>
-              <th className="border border-slate-300 md:px-2 xl:px-10 py-4 text-base bg-slate-200">
-                Quantity
-              </th>
-              <th className="border border-slate-300 md:px-2 xl:px-6 py-4 text-base bg-slate-200">
-                Total
-              </th>
-              <th className="border border-slate-300 md:px-2 xl:px-10 py-4 text-base bg-slate-200">
-                Remove
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <TableData />
-            <tr>
-              <th></th>
-              <th></th>
-              <th></th>
-              <th className="p-4">
-              </th>
-              <th className="p-4">
-                <button
-                  style={{
-                    color: "white",
-                    height: "39px",
-                    width: "150px",
-                  }}
-                  className="text-xs bg-black hover:bg-green-600"
-                  onClick={() => {
-                    router.push('/');
-                  }}
-                >
-                  CONTINUE SHOPPING
-                </button>
-              </th>
-              <th className="p-4">
-                <button
-                  style={{
-                    color: "white",
-                    height: "39px",
-                    width: "120px",
-                  }}
-                  
-                  className="text-xs bg-black hover:bg-green-600"
-                  onClick={() => {
-                    dispatch(deleteCart());
-                  }}
-                >
-                  CLEAR CART
-                </button>
-              </th>
-            </tr>
-          </tbody>
-        </table>
+      <div className="container px-4 py-20">
+        <div className="">
+          <table className="w-full border-collapse border border-slate-400">
+            <thead className="">
+              <tr>
+                <th className="border border-slate-300 bg-slate-200 px-16 py-4 text-base md:px-8">
+                  Image
+                </th>
+                <th className="border border-slate-300 bg-slate-200 py-4 text-base md:px-2 xl:px-10">
+                  Product
+                </th>
+                <th className="border border-slate-300 bg-slate-200 px-10 py-4 text-base">
+                  Price
+                </th>
+                <th className="border border-slate-300 bg-slate-200 py-4 text-base md:px-2 xl:px-10">
+                  Quantity
+                </th>
+                <th className="border border-slate-300 bg-slate-200 py-4 text-base md:px-2 xl:px-6">
+                  Total
+                </th>
+                <th className="border border-slate-300 bg-slate-200 py-4 text-base md:px-2 xl:px-10">
+                  Remove
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <TableData />
+              <tr>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th className="p-4"></th>
+                <th className="p-4">
+                  <button
+                    style={{
+                      color: 'white',
+                      height: '39px',
+                      width: '150px',
+                    }}
+                    className="bg-black text-xs hover:bg-green-600"
+                    onClick={() => {
+                      router.push('/');
+                    }}
+                  >
+                    CONTINUE SHOPPING
+                  </button>
+                </th>
+                <th className="p-4">
+                  <button
+                    style={{
+                      color: 'white',
+                      height: '39px',
+                      width: '120px',
+                    }}
+                    className="bg-black text-xs hover:bg-green-600"
+                    onClick={() => {
+                      dispatch(deleteCart());
+                    }}
+                  >
+                    CLEAR CART
+                  </button>
+                </th>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
--- packages/storefront/components/cart/subcomponents/deliveryDetails/main.tsx
+++ packages/storefront/components/cart/subcomponents/deliveryDetails/main.tsx
@@ -1,25 +1,25 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import type { NextComponentType } from "next";
+import type { NextComponentType } from 'next';
 
-import DeliveryDate from "@/components/cart/subcomponents/deliveryDetails/date";
-import CartTotal from "@/components/cart/subcomponents/deliveryDetails/cartTotal";
-import DeilveryInstructions from "@/components/cart/subcomponents/deliveryDetails/instruction";
+import DeliveryDate from '@/components/cart/subcomponents/deliveryDetails/date';
+import CartTotal from '@/components/cart/subcomponents/deliveryDetails/cartTotal';
+import DeilveryInstructions from '@/components/cart/subcomponents/deliveryDetails/instruction';
 
 const DeilveryDetails: NextComponentType = () => {
   const [fill, setFill] = useState(false);
   return (
     <>
-      <div className="grid grid-cols-1 xl:grid-rows-2 xl:grid-cols-2 lg:grid-cols-2 gap-x-4 gap-y-4">
+      <div className="container grid grid-cols-1 gap-x-4 gap-y-4 lg:grid-cols-2 xl:grid-cols-2 xl:grid-rows-2">
         {/* <div className="px-4">
           <DeliveryDate/>
         </div> */}
-       
+
         <div className="px-4">
           <DeilveryInstructions />
         </div>
         <div className="px-4">
-          <CartTotal/>
+          <CartTotal />
         </div>
       </div>
     </>
