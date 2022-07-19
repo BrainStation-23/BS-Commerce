2479988b504f2051f34e07f868897182e8a220e8
SeefatHimel <Seefat.hossain@brainstation-23.com>
2022-07-19 14:46:13 +0600

    fix product by category id

--- packages/storefront/components/cateoryProducts/categoryProducts/categoryProducts.tsx
+++ packages/storefront/components/cateoryProducts/categoryProducts/categoryProducts.tsx
@@ -11,12 +11,17 @@ const CategoryProductSegment: NextComponentType = () => {
   return (
     <>
       <div>
-        <div className="grid grid-cols-1 justify-items-center gap-2 md:grid-cols-2 md:gap-2 lg:grid-cols-3 lg:gap-3 xl:grid-cols-3 xl:gap-3">
-          {products[0] &&
-            products.map((product, index) => (
+        {products[0] ? (
+          <div className="grid grid-cols-1 justify-items-center gap-2 md:grid-cols-2 md:gap-2 lg:grid-cols-3 lg:gap-3 xl:grid-cols-3 xl:gap-3">
+            {products.map((product, index) => (
               <CategoryProductCard key={product.id} product={product} />
             ))}
-        </div>
+          </div>
+        ) : (
+          <p className="m-auto text-center">
+            There is no product in this category
+          </p>
+        )}
       </div>
     </>
   );
