6b26d205348a4f303383d36f8c7acd174831833d
ismail0946 <Ismail.hosen@brainstation-23.com>
2022-07-19 15:33:55 +0600

    static admin data changes

--- packages/seeder/src/admin/admin.seeder.ts
+++ packages/seeder/src/admin/admin.seeder.ts
@@ -1,21 +1,21 @@
-import { UserModel } from "./admin.model";
+import { UserModel } from './admin.model';
 import * as bcrypt from 'bcrypt';
 
 const admins = [
     {
-        id: "33d9cad4-b68d-4fc9-ac61-e0f7e5ba2727",
-        firstName: "John",
-        lastName: "Doe",
-        email: "johndoe@gmail.com",
-        password: "admin@123"
+        id: '33d9cad4-b68d-4fc9-ac61-e0f7e5ba2727',
+        firstName: 'John',
+        lastName: 'Doe',
+        displayName: 'John Doe',
+        username: 'johndoe@gmail.com',
+        email: 'johndoe@gmail.com',
+        password: '$2b$10$kkgek0Jew5ooU6le1XsLluvakefv/AS5pRaAGE/oWImWgCG6sUBN6', // Plain Password is "admin@123"
+        provider: 'local'
     }
 ];
 
 const seed = async () => {
     await UserModel.collection.drop();
-    admins.forEach(async admin => {
-        admin.password = await bcrypt.hash(admin.password, 10);
-    })
     await UserModel.insertMany(admins);
     console.log('Completed Admin Data seeding');
 };
