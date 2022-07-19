fa1ef70f101232b6886054667342713cbf775471
sakibbye <66922504+sakibbye@users.noreply.github.com>
2022-07-19 17:45:00 +0600

    Multilevel Ui

--- packages/storefront/components/global/components/headerCategory.tsx
+++ packages/storefront/components/global/components/headerCategory.tsx
@@ -4,33 +4,37 @@ import React from 'react';
 export const HeaderCategory: React.FC<any> = (props: any) => {
   const category = props.category;
   return (
-    <div
-      key={category.name}
-      className="group flex flex-row justify-between text-sm"
-    >
-      <Link href={category.link} as={`/collections/${category.name}`}>
-        <a className="cursor-pointer capitalize transition-all duration-100 ease-linear hover:text-green-600">
-          {category.name}
-        </a>
-      </Link>
-      <div className="">
-        {category.hasSubmenu && (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-3 w-3"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
-              clipRule="evenodd"
-            />
-          </svg>
-        )}
+    <div className="group">
+      <div
+        key={category.name}
+        className="flex flex-row justify-between text-sm"
+      >
+        <Link href={category.link} as={`/collections/${category.name}`}>
+          <a className="cursor-pointer capitalize transition-all duration-100 ease-linear hover:text-green-600">
+            {category.name}
+          </a>
+        </Link>
+        <div className="">
+          {category.hasSubmenu && (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M9 5l7 7-7 7"
+              />
+            </svg>
+          )}
+        </div>
       </div>
       {category.hasSubmenu && (
-        <div className="relative hidden overflow-hidden bg-white px-6 py-6 shadow-lg transition-all duration-300 ease-in lg:group-hover:block">
+        <div className="absolute right-[50px] top-0 z-50 hidden overflow-hidden bg-white px-6 py-6 shadow-lg transition-all duration-300 ease-in lg:group-hover:block">
           <ul className="">
             {category.submenu?.map((menu) => (
               <li
