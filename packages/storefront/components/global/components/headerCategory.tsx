42be168bf47ffba8e64f0c2d1e75229a6e624f49
SeefatHimel <Seefat.hossain@brainstation-23.com>
2022-07-20 14:21:04 +0600

    category done

--- packages/storefront/components/global/components/headerCategory.tsx
+++ packages/storefront/components/global/components/headerCategory.tsx
@@ -1,13 +1,14 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import { HeaderSubCategory } from './headerSubCategory';
 
 export const HeaderCategory: React.FC<any> = (props: any) => {
   const category = props.category;
+  const [subOff, settt] = useState(false);
   return (
     <>
       {category ? (
-        <div className="group">
+        <div className="group relative">
           <div
             key={category.name}
             className="flex flex-row justify-between text-sm"
@@ -26,34 +27,31 @@ export const HeaderCategory: React.FC<any> = (props: any) => {
                 {category.name}
               </a>
             </Link>
+            {category.subCategories ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-4 w-4"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M9 5l7 7-7 7"
+                />
+              </svg>
+            ) : (
+              ''
+            )}
             {console.log(category)}
-
-            <div className="">
-              {category.subCategories ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-4 w-4"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  stroke-width="2"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M9 5l7 7-7 7"
-                  />
-                </svg>
-              ) : (
-                ''
-              )}
-            </div>
           </div>
           {category.subCategories ? (
-            <div className=" absolute -right-[130px] top-[0] z-50 hidden bg-white px-6 py-6 shadow-lg transition-all duration-300 ease-in hover:block lg:group-hover:block">
+            <div className=" absolute  -right-[180px] -top-[10px] z-50 hidden bg-white px-10 py-6 shadow-lg transition-all duration-300 ease-in hover:block lg:group-hover:block">
               <ul className="">
                 {category.subCategories?.map((subCategory) => (
-                  <HeaderSubCategory category={subCategory} />
+                  <HeaderSubCategory category={subCategory} subOff={subOff} />
                 ))}
               </ul>
             </div>
