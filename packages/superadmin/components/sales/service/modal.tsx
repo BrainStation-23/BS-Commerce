db187d20aa726262cb67153c54a5def1e40ae142
AnikBS <anik.barua@brainstation-23.com>
2022-07-23 02:26:43 +0600

    modified button style

--- packages/superadmin/components/sales/service/modal.tsx
+++ packages/superadmin/components/sales/service/modal.tsx
@@ -1,19 +1,19 @@
-import React from "react";
+import React from 'react';
 
 interface STATUS {
   state: string;
 }
 
-const Modal = ({ state, handleStatus, handlePositive}: any) => {
+const Modal = ({ state, handleStatus, handlePositive }: any) => {
   return (
     <div
       className="modal"
-      style={{ display: `{modal.${state}` ? "block" : "none" }}
+      style={{ display: `{modal.${state}` ? 'block' : 'none' }}
     >
       <div
         className="modal-backdrop"
         style={{
-          backgroundColor: "rgba(0, 0, 0, 0.1)",
+          backgroundColor: 'rgba(0, 0, 0, 0.1)',
         }}
         onClick={() =>
           // close modal when outside of modal is clicked
@@ -27,14 +27,14 @@ const Modal = ({ state, handleStatus, handlePositive}: any) => {
             e.stopPropagation();
           }}
           style={{
-            textAlign: "left",
-            width: "30%",
-            marginLeft: "40%",
-            marginTop: "5%",
-            border: "1px solid gray",
-            boxShadow: "1px 1px 10px gray",
-            borderRadius: "10px",
-            padding: "20px",
+            textAlign: 'left',
+            width: '30%',
+            marginLeft: '40%',
+            marginTop: '5%',
+            border: '1px solid gray',
+            boxShadow: '1px 1px 10px gray',
+            borderRadius: '10px',
+            padding: '20px',
           }}
         >
           <div className="container">
@@ -46,23 +46,23 @@ const Modal = ({ state, handleStatus, handlePositive}: any) => {
             <div className="clearfix">
               <button
                 type="button"
+                className="btn btn-primary"
+                style={{marginRight: '10px', width: '70px'}}
+                onClick={() => handlePositive()}
+              >
+                Yes
+              </button>
+              <button
+                type="button"
                 className="btn btn-light"
                 style={{
-                  border: "1px solid gray",
-                  backgroundColor: "gray",
-                  color: "white",
-                  marginRight: "10px",
+                  border: '1px solid gray',
+                  backgroundColor: 'gray',
+                  color: 'white',
                 }}
                 onClick={() => handleStatus()}
               >
                 Cancel
-              </button>
-              <button
-                type="button"
-                className="btn btn-secondary"
-                onClick={() => handlePositive()}
-              >
-                Yes
               </button>
             </div>
           </div>
