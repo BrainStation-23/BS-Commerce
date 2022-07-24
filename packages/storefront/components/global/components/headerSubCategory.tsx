834ae71779f10ad85e040f5f348a0f25e96f6380
NafiAsib <nafi.asib@gmail.com>
2022-07-25 00:58:31 +0600

    fix(storefront): subcategory small screen view

--- packages/storefront/components/global/components/headerSubCategory.tsx
+++ packages/storefront/components/global/components/headerSubCategory.tsx
@@ -1,17 +1,27 @@
 import Link from 'next/link';
 import React, { useState } from 'react';
-import { ChevronRightIcon } from './headerIcons';
+import { ChevronRightIcon, MinusSolidIcon, PlusSolidIcon } from './headerIcons';
 
 interface Props {
   category: any;
   subOff?: boolean;
+  level: number;
 }
 
 export const HeaderSubCategory: React.FC<Props> = ({
   category,
   subOff,
+  level,
 }: Props) => {
   const [showSubCategory, setShowSubCategory] = useState(subOff);
+  const [expand, setExpand] = useState<boolean>(false);
+
+  const handleExpandClick = (
+    e: React.MouseEvent<HTMLSpanElement, MouseEvent>
+  ) => {
+    e.stopPropagation();
+    setExpand(!expand);
+  };
 
   // const toggleShowSubCategory = (show: boolean) => {
   //   if (show === false) {
@@ -30,17 +40,13 @@ export const HeaderSubCategory: React.FC<Props> = ({
     <>
       {category ? (
         <div
-          className="group "
-          // style={{ outline: '1px solid red' }}
+          className="group w-full"
+          onMouseEnter={() => setShowSubCategory(true)}
+          onMouseLeave={() => setShowSubCategory(false)}
         >
-          <li
-            key={category.name}
-            className="w-full cursor-pointer text-sm transition-all duration-100 ease-linear hover:text-green-600"
-            onMouseEnter={() => setShowSubCategory(true)}
-            onMouseLeave={() => setShowSubCategory(false)}
-          >
-            <div className="flex cursor-pointer flex-row items-center justify-between px-3 py-1 transition-all duration-100 ease-linear hover:text-green-600">
-              <Link
+          <div className="flex cursor-pointer flex-row items-center justify-between px-3 py-1 text-sm transition-all duration-100 ease-linear hover:text-green-600">
+            <span className="grow">
+              {/* <Link
                 href={{
                   pathname: `/collections/${category.name}`,
                   query: {
@@ -50,35 +56,69 @@ export const HeaderSubCategory: React.FC<Props> = ({
                 }}
                 //as={`/collections/${category.name}`}
               >
-                <a className="">{category.name}</a>
-              </Link>
-              {/* {console.log(category)} */}
+              </Link> */}
+              <a className={`${`ml-` + level} lg:ml-0`}>{category.name}</a>
+            </span>
 
+            <span className="hidden lg:block">
               {category.subCategories ? <ChevronRightIcon /> : ''}
-            </div>
-          </li>
-
-          {category.subCategories ? (
-            <div
-              className={`absolute top-0 left-56 z-50 h-60 w-56 bg-white shadow-lg transition-all duration-300 ease-in hover:block ${
-                showSubCategory ? '' : 'hidden'
-              }`}
+            </span>
+            <span
+              className="block lg:hidden"
+              onClick={(e: React.MouseEvent<HTMLSpanElement, MouseEvent>) =>
+                handleExpandClick(e)
+              }
             >
-              <ul className="">
-                {category.subCategories?.map((category: any) => (
+              {category.subCategories ? (
+                expand ? (
+                  <MinusSolidIcon size={4} />
+                ) : (
+                  <PlusSolidIcon size={4} />
+                )
+              ) : (
+                ''
+              )}
+            </span>
+          </div>
+
+          {/* render on lg+ screen */}
+          <div className="hidden lg:block">
+            {category.subCategories ? (
+              <div
+                className={`absolute top-0 left-56 z-50 h-auto w-56 bg-white shadow-lg transition-all duration-300 ease-in hover:block lg:h-60 ${
+                  showSubCategory ? '' : 'hidden'
+                }`}
+              >
+                <ul className="">
+                  {category.subCategories?.map((category: any) => (
+                    <li key={category.name}>
+                      <HeaderSubCategory
+                        category={category}
+                        level={level + 1}
+                        // showSubCategory1={showSubCategory}
+                      />
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ) : (
+              ''
+            )}
+          </div>
+          {/* render on sm screen */}
+          <div className={`lg:hidden`}>
+            {category.subCategories && expand ? (
+              <ul>
+                {category.subCategories.map((category: any) => (
                   <li key={category.name}>
-                    <HeaderSubCategory
-                      category={category}
-                      // showSubCategory1={showSubCategory}
-                    />
+                    <HeaderSubCategory category={category} level={level + 1} />
                   </li>
                 ))}
               </ul>
-            </div>
-          ) : (
-            // }
-            ''
-          )}
+            ) : (
+              ''
+            )}
+          </div>
         </div>
       ) : (
         ''
