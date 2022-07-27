0964a6e31457843fdb3ffcc2e9a659d9df47827a
SeefatHimel <Seefat.hossain@brainstation-23.com>
2022-07-27 14:16:03 +0600

    fix home swiper allignment

--- packages/storefront/components/global/components/header.tsx
+++ packages/storefront/components/global/components/header.tsx
@@ -219,29 +219,6 @@ const Header: NextComponentType = () => {
           </div>
         </div>
       </nav>
-      <button
-        type="button"
-        onClick={() =>
-          window.scrollTo({
-            top: 0,
-            behavior: 'smooth',
-          })
-        }
-        className={`fixed right-0  bottom-10 z-50 float-right mr-10 border-0 py-2 px-10 font-bold text-white `}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-12 w-12 rounded-full bg-white "
-          viewBox="0 0 20 20"
-          fill="#40a944 "
-        >
-          <path
-            fillRule="evenodd"
-            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13a1 1 0 102 0V9.414l1.293 1.293a1 1 0 001.414-1.414z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </button>
     </>
   );
 };
