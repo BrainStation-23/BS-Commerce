53e6b7a213c2348f2082c4cebc0ba7524c30d0ab
sbswarna <shatabdibiswasswarna@gmail.com>
2022-07-28 12:56:31 +0600

    modal and comparison

--- packages/storefront/components/global/components/modal/cartModal.tsx
+++ packages/storefront/components/global/components/modal/cartModal.tsx
@@ -1,15 +1,18 @@
-import { Product } from 'models';
+import { useAppDispatch } from 'customHooks/hooks';
+import { CustomerProduct, Product, WishlistProduct } from 'models';
 import Image from 'next/image';
 import Link from 'next/link';
+import { setCartModalState } from 'toolkit/modalSlice';
 import { XCircleIcon } from '../headerIcons';
 
 interface Props {
   open: boolean;
   onClose: () => void;
-  product: Product;
+  product?: Product | CustomerProduct | WishlistProduct;
 }
 
 const CartModal: React.FC<Props> = ({ open, onClose, product }: Props) => {
+  const dispatch = useAppDispatch();
   if (!open) return null;
   return (
     <div
@@ -17,7 +20,10 @@ const CartModal: React.FC<Props> = ({ open, onClose, product }: Props) => {
       aria-labelledby="modal-title"
       role="dialog"
       aria-modal="true"
-      onClick={onClose}
+      onClick={() => {
+        dispatch(setCartModalState({ showModal: false }));
+        onClose();
+      }}
     >
       <div className="fixed inset-0 bg-neutral-900/40 bg-opacity-75 transition-opacity"></div>
 
@@ -29,17 +35,23 @@ const CartModal: React.FC<Props> = ({ open, onClose, product }: Props) => {
             }
             className="relative flex transform flex-row bg-white p-4 text-left shadow-xl transition-all"
           >
-            <button className="absolute -right-7 -top-5 z-50" onClick={onClose}>
+            <button
+              className="absolute -right-7 -top-5 z-50"
+              onClick={() => {
+                dispatch(setCartModalState({ showModal: false }));
+                onClose();
+              }}
+            >
               <XCircleIcon size={8} />
             </button>
             <Image
-              src={product.photos![0].url!}
-              alt={product.photos![0].alt || 'product image'}
+              src={product?.photos![0].url!}
+              alt={product?.photos![0].alt || 'product image'}
               width={141}
               height={141}
             />
             <div className="flex w-80 flex-col px-4">
-              <span className="mb-4">{product.info.name}</span>
+              <span className="mb-4">{product?.info.name}</span>
               <div className="mb-8 flex flex-row text-green-600">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -62,7 +74,10 @@ const CartModal: React.FC<Props> = ({ open, onClose, product }: Props) => {
                 <Link href="/cart" passHref>
                   <button
                     className="rounded-md bg-gray-200/70 py-2 px-4 uppercase transition-all duration-200 ease-linear hover:bg-green-600 hover:text-white"
-                    onClick={onClose}
+                    onClick={() => {
+                      dispatch(setCartModalState({ showModal: false }));
+                      onClose();
+                    }}
                   >
                     View Cart
                   </button>
@@ -70,7 +85,10 @@ const CartModal: React.FC<Props> = ({ open, onClose, product }: Props) => {
                 <Link href="/checkout" passHref>
                   <button
                     className="ml-3 rounded-md bg-gray-200/70 py-2 px-4 uppercase transition-all duration-200 ease-linear hover:bg-green-600 hover:text-white"
-                    onClick={onClose}
+                    onClick={() => {
+                      dispatch(setCartModalState({ showModal: false }));
+                      onClose();
+                    }}
                   >
                     Checkout
                   </button>
--- packages/storefront/toolkit/modalSlice.ts
+++ packages/storefront/toolkit/modalSlice.ts
@@ -1,13 +1,23 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { CustomerProduct, Product, WishlistProduct } from "models";
+
+export interface CartModalState {
+  showModal: boolean,
+  product?: Product | WishlistProduct | CustomerProduct | null
+}
 
 export interface ModalState {
   setModal: boolean;
   setModalWishlist: boolean
+  setModalCart: CartModalState
 }
 
 const initialState: ModalState = {
   setModal: false,
-  setModalWishlist: false
+  setModalWishlist: false,
+  setModalCart: {
+    showModal: false
+  },
 };
 
 export const modalSlice = createSlice({
@@ -20,9 +30,16 @@ export const modalSlice = createSlice({
     setWishlistModalState: (state: ModalState, action: PayloadAction<boolean>) => {
       state.setModalWishlist = action.payload;
     },
+    setCartModalState: (state: ModalState, action: PayloadAction<CartModalState>) => {
+      const modalObj = {
+        showModal: action.payload.showModal,
+        product: action.payload.product ? action.payload.product : null
+      }
+      state.setModalCart = modalObj;
+    },
   },
 });
 
-export const { setModalState, setWishlistModalState } = modalSlice.actions;
+export const { setModalState, setWishlistModalState, setCartModalState } = modalSlice.actions;
 
 export default modalSlice.reducer;
