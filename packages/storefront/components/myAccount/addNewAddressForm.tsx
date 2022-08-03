fe02cb8787c6f37c0fade5f7a84beed750d86bb9
SeefatHimel <Seefat.hossain@brainstation-23.com>
2022-08-03 12:14:12 +0600

    fix profile address

--- packages/storefront/components/myAccount/addNewAddressForm.tsx
+++ packages/storefront/components/myAccount/addNewAddressForm.tsx
@@ -247,7 +247,7 @@ const AddNewAddressForm: FC<props> = ({ user, cancelForm, id }: props) => {
                   type="submit"
                   className="my-2 w-full rounded bg-green-600/100 py-2 text-white hover:bg-black sm:w-full md:w-32 lg:w-32 xl:w-32"
                 >
-                  {user?.firstName ? 'Edit ' : 'Add '} Address
+                  {user?.firstName ? 'Save ' : 'Add '} Address
                 </button>
                 <br />
               </Form>
