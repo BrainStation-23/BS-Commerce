bc25c7bb98d0c5921c6bf08bceeaba2a1e9033e7
ismail0946 <Ismail.hosen@brainstation-23.com>
2022-08-04 19:48:55 +0600

    tag module test case done

--- packages/headless/test/modules/tags/tags.predefined.data.ts
+++ packages/headless/test/modules/tags/tags.predefined.data.ts
@@ -1,13 +1,7 @@
 import { CreateTagRequestBodyDto } from 'src/modules/tags/rest/dto';
-import { TagDto } from 'src/modules/tags/rest/dto/tags.dto';
-
-export const Tag: TagDto = {
-    id: '33d9cad4-b68d-4fc9-ac61-e0f7e5ba2727',
-    name: 'phone'
-}
 
 export const CreateTagRequest: CreateTagRequestBodyDto = {
-    name: 'smartphone',
+    name: 'breakfast',
 };
 
 export const CreateTagRequestWithoutName = {}
