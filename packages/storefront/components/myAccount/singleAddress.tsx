ac2ea6bc0990b1db0c6cb4993d58e0139dd83328
SeefatHimel <Seefat.hossain@brainstation-23.com>
2022-08-04 14:55:46 +0600

    fix adreess raio button color

--- packages/storefront/components/myAccount/singleAddress.tsx
+++ packages/storefront/components/myAccount/singleAddress.tsx
@@ -31,7 +31,7 @@ const SingleAddress: React.FC<Props> = ({ singleAddress }) => {
     <>
       <div>
         <div>
-          <div className="mt-3 text-sm relative lg:w-2/3">
+          <div className="relative mt-3 text-sm lg:w-2/3">
             <div className="md:flex md:flex-row md:justify-between">
               <p>
                 {singleAddress?.firstName!} {singleAddress?.lastName!}
@@ -50,8 +50,8 @@ const SingleAddress: React.FC<Props> = ({ singleAddress }) => {
                   className="peer sr-only"
                   checked={checked}
                 />
-                <div className="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:top-0.5 after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-4 peer-focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800"></div>
-                <span className="hidden md:block ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+                <div className="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:top-0.5 after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-green-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-4 peer-focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800"></div>
+                <span className="ml-3 hidden text-sm font-medium text-gray-900 dark:text-gray-300 md:block">
                   Default
                 </span>
               </label>
