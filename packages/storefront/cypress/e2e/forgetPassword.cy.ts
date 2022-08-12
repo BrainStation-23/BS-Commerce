0a6f5057f9667cb3dd2a5e0dd808206002e544d4
sbswarna <shatabdibiswasswarna@gmail.com>
2022-08-12 10:18:23 +0600

    e2e test for forget password (incomplete)

--- packages/storefront/cypress/e2e/forgetPassword.cy.ts
+++ packages/storefront/cypress/e2e/forgetPassword.cy.ts
@@ -1,27 +1,42 @@
 describe('Forget Password Journey', () => {
-    it('should be redirected to forget password page', () => {
-      cy.visit('/account/sign-in');
-      cy.get('#forgotPasswordDiv').click();
-      cy.url().should('eq', Cypress.config().baseUrl + '/account/forgot-password');
-      cy.get('#username').type("sbswarna@gmail.com");
-      cy.contains('button', 'Submit').click();
-      cy.get('#otp').type("1234");
-      cy.contains('button', 'Submit').click();
-      cy.get('#newPassword').type("123456");
-      cy.get('#confirmPassword').type("123456");
-      cy.contains('button', 'Submit').click();
-    });
+  it('should be redirected to forget password page', () => {
+    cy.visit('/');
+    cy.get('#menuicon').click();
+    cy.get('#login').click({ force: true });
+    cy.get('#sidebarClose').click();
+    cy.get('#forgotPasswordDiv').click();
+    cy.url().should(
+      'eq',
+      Cypress.config().baseUrl + '/account/forgot-password'
+    );
+    cy.get('#username').type('sbswarna@gmail.com');
+    cy.contains('button', 'Submit').click();
+    cy.get('#otp').type('1234');
+    cy.contains('button', 'Submit').click();
+    cy.get('#newPassword').type('123456');
+    cy.get('#confirmPassword').type('123456');
+    cy.contains('button', 'Submit').click();
   });
+});
 
-  describe('Cancel forget password', () => {
-    it('should be redirected to sign-in page', () => {
-      cy.visit('/account/sign-in');
-      cy.get('#forgotPasswordDiv').click();
-      cy.url().should('eq', Cypress.config().baseUrl + '/account/forgot-password');
-      cy.get('#cancelDiv').click();
-      cy.url().should('eq', Cypress.config().baseUrl + '/account/sign-in');
-    });
-  });
-  
-  export {};
-  
+// describe('Cancel forget password', () => {
+//   it('should be redirected to sign-in page', () => {
+//     cy.visit('/');
+//     cy.get('#menuicon').click();
+
+//     //Go to Login page
+//     cy.get('#login').click({ force: true });
+
+//     cy.get('#sidebarClose').click();
+
+//     cy.get('#forgotPasswordDiv').click();
+//     cy.url().should(
+//       'eq',
+//       Cypress.config().baseUrl + '/account/forgot-password'
+//     );
+//     cy.get('#cancelDiv').click();
+//     cy.url().should('eq', Cypress.config().baseUrl + '/account/sign-in');
+//   });
+// });
+
+export {};
