e3fd1dadd41b5cf4ad8fa1f995d506525aab7382
sbswarna <shatabdibiswasswarna@gmail.com>
2022-08-17 15:46:43 +0600

    E2e test

--- packages/storefront/components/account/forgetPassword/components/common/cancelButton.tsx
+++ packages/storefront/components/account/forgetPassword/components/common/cancelButton.tsx
@@ -8,7 +8,10 @@ const FormCancelButton = () => {
         className="text-decoration-none my-0 sm:my-0 md:my-3 lg:my-3 xl:my-3"
       >
         <Link href="/account/sign-in">
-          <a className="text-decoration-none font-weight-light text-gray-600 hover:text-gray-500">
+          <a
+            id="cancelForgetPassword"
+            className="text-decoration-none font-weight-light text-gray-600 hover:text-gray-500"
+          >
             Cancel
           </a>
         </Link>
