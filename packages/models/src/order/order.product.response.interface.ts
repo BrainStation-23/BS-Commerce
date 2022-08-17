adc316907d13095110d38314a5acb864d0aa71e8
tahsinZilani <shamsul.tahsin@brainstation-23.com>
2022-08-17 22:01:56 +0600

    Order Model formatted

---
+++ packages/models/src/order/order.product.response.interface.ts
@@ -0,0 +1,14 @@
+import { CreateProductOrderDetails } from './order.create.interface';
+
+export interface IOrderProductPhoto {
+    url?: string,
+    id?: string,
+    title?: string,
+    alt?: string,
+    displayOrder?: number
+}
+
+export interface IOrderProduct extends CreateProductOrderDetails{
+    photos?: IOrderProductPhoto[],
+    totalPrice: number;
+}
