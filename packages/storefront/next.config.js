ec542fe4c316dd45b0847de99a05d5a63a0a56b1
NafiAsib <nafi.asib@gmail.com>
2022-08-19 17:04:42 +0600

    feat(storefront): update next.config.js to use babel-loader when loading components from atomic-component

--- packages/storefront/next.config.js
+++ packages/storefront/next.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -6,22 +8,64 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: "/home",
-        destination: "/",
+        source: '/home',
+        destination: '/',
         permanent: true,
       },
     ];
   },
   images: {
-    domains: ["cdn.shopify.com", "dummyjson.com", "colombocme.org"],
+    domains: ['cdn.shopify.com', 'dummyjson.com', 'colombocme.org'],
   },
 };
 
-const withTM = require("next-transpile-modules")(["marketplace"]);
+const withTM = require('next-transpile-modules')(['marketplace']);
 module.exports = withTM(nextConfig);
+
+const packages = [];
+packages.push(path.join(__dirname, '../atomic-components'));
 
 module.exports = {
   images: {
-    domains: ["dummyjson.com", "cdn.shopify.com"],
+    domains: ['dummyjson.com', 'cdn.shopify.com'],
+  },
+  webpack: async (baseConfig, arg) => {
+    const { module = {} } = baseConfig;
+    const newConfig = {
+      ...baseConfig,
+      module: {
+        ...module,
+        rules: [...(module.rules || [])],
+      },
+    };
+
+    newConfig.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      include: [path.resolve(__dirname, '../atomic-components')],
+      use: [
+        {
+          loader: 'babel-loader',
+          options: {
+            presets: ['next/babel'],
+            // plugins: ['react-docgen']
+          },
+        },
+      ],
+    });
+    newConfig.resolve.extensions.push('.ts', '.tsx');
+
+    // const { isFound, match } = getLoader(
+    //   webpackConfig,
+    //   loaderByName('babel-loader')
+    // );
+    // if (isFound) {
+    //   const include = Array.isArray(match.loader.include)
+    //     ? match.loader.include
+    //     : [match.loader.include];
+
+    //   match.loader.include = include.concat(packages);
+    // }
+
+    return newConfig;
   },
 };
