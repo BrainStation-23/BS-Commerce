adfcb77ad38150b7e1287c067bb2d8efb047b44d
tahsinZilani <shamsul.tahsin@brainstation-23.com>
2022-08-23 02:49:45 +0600

    unnecessary console removed

--- packages/headless/src/database/mongodb/cart/index.ts
+++ packages/headless/src/database/mongodb/cart/index.ts
@@ -61,6 +61,7 @@ export class CartDatabase implements ICartDatabase {
   }
 
   async createCart(cart:Cart): Promise<Cart | null> {
+    // console.log(cart);
     let newCart = await CartModel.create(cart);
     return await newCart.toObject();
   }
