a875f936aa7e79d15b42a7e2222a853aff49e595
hasibulalamrahi <hasibul.alam@brainstation-23.com>
2022-08-23 17:39:40 +0600

    cached files removed

--- packages/documentation/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('C:\\Users\\BS449\\Desktop\\BS-Projects\\BS-Commerce\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('C:\\Users\\BS449\\Desktop\\BS-Projects\\BS-Commerce\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('C:\\Users\\BS449\\Desktop\\BS-Projects\\BS-Commerce\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('C:\\Users\\BS449\\Desktop\\BS-Projects\\BS-Commerce\\packages\\documentation\\src\\css\\custom.css'),
-];
