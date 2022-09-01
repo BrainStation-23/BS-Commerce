10ad3fdda5b935f5eb17deb3eea70e441acd9d3e
SeefatHimel <Seefat.hossain@brainstation-23.com>
2022-09-01 15:52:32 +0600

    fix setup

--- packages/storefront/config/index.ts
+++ packages/storefront/config/index.ts
@@ -1,7 +1,7 @@
 const { NEXT_PUBLIC_REST_API_PREFIX_GRAPHQL, NEXT_PUBLIC_REST_API_PREFIX_REST, NEXT_PUBLIC_API, NEXT_PUBLIC_SIGN_IN_URL ,NEXT_PUBLIC_REST_API_PREFIX_STATIC } = process?.env;
 
 export const config = {
-    apiService: NEXT_PUBLIC_API || 'STATIC',
+    apiService: NEXT_PUBLIC_API || 'REST',
     graphqlPrefix: NEXT_PUBLIC_REST_API_PREFIX_GRAPHQL || 'http://localhost:3000/graphql',
     restPrefix: NEXT_PUBLIC_REST_API_PREFIX_REST || 'http://localhost:3000/api',
     staticPrefix: NEXT_PUBLIC_REST_API_PREFIX_STATIC || 'http://localhost:3002/api',
