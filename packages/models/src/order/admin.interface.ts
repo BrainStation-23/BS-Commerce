68c6096fb503769d82cfcf69691c84635a06c1ae
tahsinZilani <shamsul.tahsin@brainstation-23.com>
2022-09-06 01:28:25 +0600

    added new models

--- packages/models/src/order/admin.interface.ts
+++ packages/models/src/order/admin.interface.ts
@@ -14,3 +14,34 @@ export interface OrderStat{
     yearTotal: number
     allTimeTotal: number
 }
+
+export interface OrderStatusEnumModel {
+    Pending: string;
+    Processing: string;
+    Completed: string;
+    Cancelled: string;
+}
+
+export interface PaymentStatusEnumModel {
+    Pending: string;
+    Paid: string;
+    Cancelled: string;
+}
+
+export interface ShippingStatusEnumModel {
+    NotYetShipped: string;
+    PartiallyShipped: string;
+    Shipped: string;
+    Delivered: string;
+}
+export interface OrderStatusModel {
+    orderStatusEnums: OrderStatusEnumModel;
+    paymentStatusEnums: PaymentStatusEnumModel;
+    shippingStatusEnum: ShippingStatusEnumModel;
+}
+
+export interface ChangeStatusModel { 
+    orderId: string;
+    statusType: string;
+    statusValue: string
+  }
