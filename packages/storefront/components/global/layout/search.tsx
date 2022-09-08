6592d74e76d02bdd61325e735561f7f8e05043fd
SeefatHimel <Seefat.hossain@brainstation-23.com>
2022-09-08 11:24:13 +0600

    fix home input field design

--- packages/storefront/components/global/layout/search.tsx
+++ packages/storefront/components/global/layout/search.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 interface Properties {
   placeholder: string;
@@ -7,7 +7,7 @@ interface Properties {
 const Search: React.FC<Properties> = (props) => {
   return (
     <div
-      className={`flex flex-row items-center justify-between h-12 border border-slate-200 rounded-full text-sm w-full`}
+      className={`flex h-12 w-full flex-row items-center justify-between rounded-full border border-slate-200 text-sm`}
     >
       <input
         className="ml-4 bg-white focus:outline-none"
@@ -15,7 +15,7 @@ const Search: React.FC<Properties> = (props) => {
         name="search"
         placeholder={`${props.placeholder}`}
       />
-      <div className="flex justify-center bg-green-600 w-14 h-full rounded-r-full hover:bg-stone-900 transition-all duration-200 ease-linear cursor-pointer">
+      <div className="flex h-12 w-14 cursor-pointer justify-center rounded-r-full bg-green-600 transition-all duration-200 ease-linear hover:bg-stone-900">
         <button type="submit" className="">
           <svg
             xmlns="http://www.w3.org/2000/svg"
