0cc314e7cd29f53a372880411582e54e817fe874
sbswarna <shatabdibiswasswarna@gmail.com>
2022-09-16 11:57:07 +0600

    build fix

--- packages/storeadmin/components/navbar.tsx
+++ packages/storeadmin/components/navbar.tsx
@@ -1,4 +1,5 @@
 import type { NextComponentType } from "next";
+import Link from "next/link";
 
 const Navbar: NextComponentType = () => {
   return (
@@ -10,40 +11,52 @@ const Navbar: NextComponentType = () => {
         <div className="position-sticky pt-3">
           <ul className="nav flex-column">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/home">
-                <i className="bi bi-house-door"></i>
-                Dashboard
-              </a>
+              <Link href="/home">
+                <a className="nav-link active" aria-current="page">
+                  <i className="bi bi-house-door"></i>
+                  Dashboard
+                </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/orders">
-                <i className="bi bi-file-earmark"></i>
-                Orders
-              </a>
+              <Link href="/orders">
+                <a className="nav-link">
+                  <i className="bi bi-file-earmark"></i>
+                  Orders
+                </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                <i className="bi bi-cart"></i>
-                Products
-              </a>
+              <Link href="#">
+                <a className="nav-link">
+                  <i className="bi bi-cart"></i>
+                  Products
+                </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                <i className="bi bi-people"></i>
-                Customers
-              </a>
+              <Link href="#">
+                <a className="nav-link">
+                  <i className="bi bi-people"></i>
+                  Customers
+                </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                <i className="bi bi-people"></i>
-                Reports
-              </a>
+              <Link href="#">
+                <a className="nav-link">
+                  <i className="bi bi-people"></i>
+                  Reports
+                </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                <i className="bi bi-people"></i>
-                Integrations
-              </a>
+              <Link href="#">
+                <a className="nav-link">
+                  <i className="bi bi-people"></i>
+                  Integrations
+                </a>
+              </Link>
             </li>
           </ul>
         </div>
