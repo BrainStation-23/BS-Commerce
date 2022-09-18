5ec1c8548d274f490aca8c3656a88848447c0baf
SeefatHimel <Seefat.hossain@brainstation-23.com>
2022-09-18 17:53:35 +0600

    fix dashboard

--- packages/storeadmin/pages/Manufacturer/index.tsx
+++ packages/storeadmin/pages/Manufacturer/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import List from "../../components/manufacturer/index";
 const ManufacturerLists: NextPage = () => {
   return (
-    <main className=" ms-sm-autopx-md-4">
+    <main className=" ms-sm-auto px-md-4">
       <List />
     </main>
   );
