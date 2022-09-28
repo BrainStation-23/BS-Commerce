0e7ed668efe50f161f4ce089d63f3624e191d36b
SeefatHimel <Seefat.hossain@brainstation-23.com>
2022-09-28 12:38:58 +0600

    fix faq

--- packages/storefront/components/global/components/accordion.tsx
+++ packages/storefront/components/global/components/accordion.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react";
+import React, { FC, useState } from 'react';
 interface Props {
   title: string;
   body: string;
@@ -11,21 +11,25 @@ const Accordion: FC<Props> = ({ title, body }) => {
   return (
     <div className="mb-3">
       <div
-        className={`${
-          isActive ? "bg-green-500 rounded-t-lg text-white" : ""
-        } flex justify-between p-3 cursor-pointer z-10`}
+        className={`transition-all duration-1000   ${
+          isActive ? 'rounded-t-lg bg-green-500 text-white' : ''
+        } z-10 flex cursor-pointer justify-between p-3`}
         onClick={() => setIsActive(!isActive)}
       >
         <div>{title}</div>
-        <div>{isActive ? "-" : "+"}</div>
+        <div>{isActive ? '-' : '+'}</div>
       </div>
-      {isActive && (
+      {
         <div
-          className={`p-4 border border-slate-300 border-solid animate-fade-in-down`}
+          className={`animate-fade-in-down overflow-hidden p-4 transition-all delay-300 duration-1000 ${
+            isActive
+              ? 'block h-max border border-solid border-slate-300'
+              : 'hidden h-0 delay-1000'
+          }`}
         >
           {body}
         </div>
-      )}
+      }
     </div>
   );
 };
