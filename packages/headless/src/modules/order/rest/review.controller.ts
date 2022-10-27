a5fdc05b8898a9b4a210a0eb01e8db6340ca6d3e
tahsinZilani <shamsul.tahsin@brainstation-23.com>
2022-10-27 13:04:54 +0600

    api response added in swagger

--- packages/headless/src/modules/order/rest/review.controller.ts
+++ packages/headless/src/modules/order/rest/review.controller.ts
@@ -1,16 +1,21 @@
 import { Body, Controller, Get, Param, Post, Res } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiResponse, ApiTags } from "@nestjs/swagger";
 import { OrderReviewService } from "../services/review.service";
 import { CreateReviewDto } from "./dto/create.review.dto";
 import { Response } from 'express';
-import { ProductReviewResponseDto } from "./dto/product.review.dto";
+import { ProductReviewListDto, ProductReviewResponseDto } from "./dto/product.review.dto";
 import { IServiceResponse } from "src/utils/response/service.response.interface";
+import { ReviewDto } from "./dto/review.dto";
 
 @ApiTags('Order - Review API')
 @Controller('order/review')
 export class OrderReviewController {
     constructor(private orderReviewService: OrderReviewService) {}
 
+    @ApiResponse({
+        type: ReviewDto,
+        description: 'Response of a review after being created',
+    })
     @Post()
     async createReview(
         @Body() body: CreateReviewDto,
@@ -22,6 +27,11 @@ export class OrderReviewController {
         return response;
     }
 
+
+    @ApiResponse({
+        type: ProductReviewListDto,
+        description: 'Review list of a product',
+    })
     @Get('/:productId')
     async getProductReview(
         @Param('productId') productId: string,
@@ -32,4 +42,6 @@ export class OrderReviewController {
         res.status(code);
         return response;
     }
+
+
 }
