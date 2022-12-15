88d9cbd46975fbcbf98739158d2ec6decd8159cf
Shatabdi Biswas <shatabdi.biswas@brainstation-23.com>
2022-12-15 19:10:26 +0600

    Documentation  image

--- packages/documentation/src/pages/index.js
+++ packages/documentation/src/pages/index.js
@@ -1,9 +1,9 @@
-import React from "react";
-import clsx from "clsx";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import Layout from "@theme/Layout";
+import clsx from "clsx";
+import React from "react";
 
 import styles from "./index.module.css";
 
@@ -14,25 +14,24 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={(styles.flexContainer, styles.mb)}>
-          <Link
-            className={clsx("button button--secondary button--lg", styles.mr)}
-            to="/docs/intro"
-          >
-            Storefront demo
-          </Link>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
-            Storefront admin
-          </Link>
+        <div className={(styles.mb)} style={{ display: "flex", "justify-content": "center" }}>
+          <div style={{ width: "max-content"}}>
+            <img src={require("/static/img/storefront.png").default} height={250} width={350} />
+
+            <Link className={clsx("button button--secondary button--lg", styles.mr)} to="https://bs-commerce-storefront.vercel.app/">
+              Storefront demo
+            </Link>
+          </div>
+
+          <div style={{ width: "max-content"}}>
+            <img src={require("/static/img/storeadmin.png").default} height={250} width={350} />
+            <Link className="button button--secondary button--lg" to="https://bs-commerce-storeadmin.vercel.app/">
+              Storefront admin
+            </Link>
+          </div>
         </div>
         <div className={styles.mb}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
+          <Link className="button button--secondary button--lg" to="/docs/intro">
             Static Storefront
           </Link>
         </div>
@@ -47,10 +46,7 @@ function HomepageHeader() {
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`Welcome to ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
+    <Layout title={`Welcome to ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
