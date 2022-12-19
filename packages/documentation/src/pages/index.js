1bd391385533d586d61da0308ad315311f133e41
Shatabdi Biswas <shatabdi.biswas@brainstation-23.com>
2022-12-19 10:28:22 +0600

    Doc style change

--- packages/documentation/src/pages/index.js
+++ packages/documentation/src/pages/index.js
@@ -14,10 +14,10 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={(styles.mb)} style={{ display: "flex", "justify-content": "center" }}>
+        <div className={(styles.mb)} style={{ display: "flex", "justify-content": "center", gap: "100px" }}>
           <div style={{ width: "max-content"}}>
             <img src={require("/static/img/storefront.png").default} height={250} width={350} />
-
+            <br/>
             <Link className={clsx("button button--secondary button--lg", styles.mr)} to="https://bs-commerce-storefront.vercel.app/">
               Storefront demo
             </Link>
@@ -25,6 +25,7 @@ function HomepageHeader() {
 
           <div style={{ width: "max-content"}}>
             <img src={require("/static/img/storeadmin.png").default} height={250} width={350} />
+            <br/>
             <Link className="button button--secondary button--lg" to="https://bs-commerce-storeadmin.vercel.app/">
               Storefront admin
             </Link>
