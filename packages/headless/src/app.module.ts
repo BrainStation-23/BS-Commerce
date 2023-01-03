e109e244c0b7461f2d9c0846b541040ccbf2dd59
shafi2t5 <shafiul.islam@brainstation-23.com>
2023-01-03 15:37:07 +0600

    changes on env

--- packages/headless/src/app.module.ts
+++ packages/headless/src/app.module.ts
@@ -3,14 +3,8 @@ import { HelperModule } from './helper/helper.module';
 import { ResolveGraphqlModule } from './internal/graphql/graphql.module.resolver';
 import { ResolveRestModule } from './internal/rest/rest.module.resolver';
 import { coreConfig } from 'config/core';
-import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: process.env.NODE_ENV
-        ? `${process.cwd()}/${process.env.NODE_ENV}.env`
-        : 'development.env',
-    }),
     HelperModule,
     ...(coreConfig.api === 'GRAPHQL'
       ? ResolveGraphqlModule()
